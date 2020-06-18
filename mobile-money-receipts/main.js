413828a3cf3f12be61a5d932aaa2dae5b5d616d3
Angello Obel <angello@pop-os.localdomain>
2020-06-18 16:09:05 +0300

    moved translations to their own file

--- mobile-money-receipts/main.js
+++ mobile-money-receipts/main.js
@@ -8,9 +8,13 @@ for (var i = 0; i < arrayLength; i++) {
     }
 }
 
+var receipt = getTranslation('payment_receipt_ug', {
+    firstName: client.FirstName,
+    lastTransaction: contact.vars.lastTransactionAmount,
+    balance: earliestBalance
+}, 'en')
 
-var receipt = "Hello " + client.FirstName + ", You have paid OAF " + contact.vars.lastTransactionAmount + ". Your Loan Balance is now " + LoanBalance + ". For questions, call 0800388889. Thank You!";
 sendMessage({ 
     "to_number": contact.phone_number,
-    "content" : receipt
+    "content": receipt
  });
