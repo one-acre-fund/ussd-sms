39c10d7c151b7ac0933651a4d08fc2710cf4d5bb
Angello Obel <angello@pop-os.localdomain>
2020-06-18 16:09:05 +0300

    moved translations to their own file

---
+++ mobile-money-receipts/translations/test.js
@@ -0,0 +1,11 @@
+const translations = require(".");
+
+const getTranslations = require('./index');
+
+describe('getTranslations', () => {
+  it('should provide an english translation fron payment_receipt_ug', () => {
+    const options = { firstName: 'Angello', lastTransaction: 31415, balance: 51427 }
+    const expectedMessage = `Hello ${options.firstName}, You have paid OAF ${options.lastTransaction}. Your Loan Balance is now ${options.balance}. For questions, call 0800388889. Thank You!`;
+    expect(getTranslations('payment_receipt_ug', options, 'en')).toEqual(expectedMessage);
+  });
+});
