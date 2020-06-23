92b7c8344b2543c4c2850482945f36f316a4ba42
Angello Obel <angello.obel@oneacrefund.org>
2020-06-23 12:28:59 +0300

    added telerivet global variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,9 +23,23 @@ module.exports = {
             'error'
         ]
     },
-    'globals': {
+    'globals': { // these are  telerivet global variables
         contact: 'readonly',
-        sendMessage: 'readonly'
+        sendMessage: 'readonly',
+        curssor: 'readonly',
+        cursor: 'readonly',
+        moment: 'readonly',
+        console: 'readonly',
+        state: 'readonly',
+        call: 'readonly',
+        promptDigits: 'readonly',
+        sayText: 'readonly',
+        '_': 'readonly',
+        service: 'readonly',
+        project: 'readonly',
+        addInputHandler: 'readonly',
+        stopRules: 'readonly',
+        httpClient: 'readonly',
     },
     'overrides': [
         {
