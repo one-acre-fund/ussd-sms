9f16b70d6c75f3d073a5ab80b53f8fe389f96415
Angello Obel <angello.obel@oneacrefund.org>
2020-06-25 11:34:47 +0300

    Only alert if duplicate bundleIds found
    this should stop the alerts

--- rw-legacy/lib/enr-get-product-options.js
+++ rw-legacy/lib/enr-get-product-options.js
@@ -2,36 +2,42 @@
 module for retrieving product option rows
 */
 
-const districtBundles = require('../dat/district-bundles');
+var districtBundles = require('../dat/district-bundles');
+var admin_alert = require('./admin-alert');
+
 module.exports = function(product_name, product_table){
-    console.log(product_name + ' : ' + product_table)
+    console.log(product_name + ' : ' + product_table);
     var selections = districtBundles.filter(function (row) {
-        return row.bundle_name == product_name
-    })
+        return row.bundle_name == product_name;
+    });
     if(selections.length>0){
         var product_row = selections[0];
         if(selections.length>1){
-            admin_alert = require('./admin-alert');
-            admin_alert('Duplicate product in District bundles : ' + product_name);
+            var bundleIds = {};
+            selections.forEach(function (row) {
+                bundleIds[row.bundleId] = '';
+            });
+            if(Object.keys(bundleIds).length < selections.length){
+                admin_alert('Duplicate product in District bundles : ' + product_name);
+            }
         }
         if(product_row.bundle_name == null){
-            admin_alert = require('./admin-alert');
             admin_alert('Null product in District bundles : ' );
-            throw 'ERROR: null product'
+            throw 'ERROR: null product';
         }
         return {'input_name': product_row.input_name,
-                'bundle_name': product_row.bundle_name,
-                'increment' : product_row.increment,
-                'price'     : product_row.price,
-                'max'       : product_row.max,
-                'min'       : product_row.min,
-                'unit'      : product_row.unit,
-                'en'        : product_row.en,
-                'ki'        : product_row.ki,
-                'bundleId'  : product_row.bundleId,
-                'bundleInputId'  : product_row.bundleInputId,
-                'acceptableQuantityList'  : product_row.acceptableQuantityList
-            }
+            'bundle_name': product_row.bundle_name,
+            'increment': product_row.increment,
+            'price': product_row.price,
+            'max': product_row.max,
+            'min': product_row.min,
+            'unit': product_row.unit,
+            'en': product_row.en,
+            'ki': product_row.ki,
+            'bundleId': product_row.bundleId,
+            'bundleInputId': product_row.bundleInputId,
+            'acceptableQuantityList': product_row.acceptableQuantityList
+        };
     }
     else{
         throw 'ERROR: missing product in district bundles : ' + product_name;
