88c55c0f043631714fa5fbdd876c6d6c262f07a8
Angello Obel <angello.obel@oneacrefund.org>
2020-07-08 09:29:55 +0300

    better date formatting

--- transaction-history/list-transactions/index.js
+++ transaction-history/list-transactions/index.js
@@ -9,7 +9,7 @@ var listTransactions = function (transactionHistory, page, errorMessage) {
     var to_show = transactionHistory.slice(offset, offset + 4);
     var options_list = (errorMessage || '')+ translate('select_payment_detail_prompt')+'\n';
     to_show.forEach(function (transaction, index) {
-        var date = transaction.RepaymentDate.substring(0, 10).replace(/\//g, '-');
+        var date = transaction.RepaymentDate.split(' ')[0].replace(/\//g, '-');
         options_list = options_list + translate('payment_list_item', {
             '$option': offset + index + 1,
             '$date': date,
@@ -25,7 +25,7 @@ module.exports = {
     list: listTransactions,
     show: function (transaction) {
         var translate = createTranslator(translations, project.vars.lang);
-        var RepaymentDate = transaction.RepaymentDate.substring(0,10).replace( /\//g,'-');
+        var RepaymentDate = transaction.RepaymentDate.split(' ')[0].replace( /\//g,'-');
         
         sayText(translate('payment_detail',{
             '$RepaymentId': transaction.RepaymentId,
