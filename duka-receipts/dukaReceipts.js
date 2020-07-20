990e7f36da1cf8eeeb03e5450fec8f3fab0a4cfa
BAHATI Robben <bahatiroben@gmail.com>
2020-07-20 09:35:29 +0200

    fixing the calculation of total cost and vat cost

--- duka-receipts/dukaReceipts.js
+++ duka-receipts/dukaReceipts.js
@@ -6,22 +6,25 @@
     }
     var getObjectKeys = require('./objectKeysFilter');
     var response = state.vars;
-    var netPriceKeys = getObjectKeys(response, 'netUnitPrice');
-    var rowPriceKeys = getObjectKeys(response, 'rowPrice');
+    var netPriceKeys = getObjectKeys(response, /^netUnitPrice?/);
+    var rowPriceKeys = getObjectKeys(response, /^rowPrice?/);
     var totalCost = 0;
     var productCost = 0;
     
     netPriceKeys.forEach(function(key){
-        totalCost = totalCost + Number(response[key]); 
+        var postFixNumber = key.match(/\d+/)[0];
+        var quantity = 'quantity' + postFixNumber;
+        totalCost = totalCost + (Number(response[key]) * Number(response[quantity])); 
     });
     
     rowPriceKeys.forEach(function(key){
         productCost = productCost + Number(response[key]); 
     });
     var vatCost = totalCost - productCost;
-    var SMStext = 'Thank you for shopping at the OAF Duka! Date: ' + response.date + ' Invoice nro: ' + response.receipt + ' Product cost: KES ' + productCost + ' VAT: KES ' + vatCost + ' Total: KES ' + totalCost;
+    var SMStext = 'Thank you for shopping at the OAF Duka! Date: ' + response.date + ' Invoice nro: ' + response.receipt + ' Product cost: KES ' + productCost + ' VAT: KES ' + vatCost.toFixed(2) + ' Total: KES ' + totalCost;
     var RouteIDPush = project.vars.route_push;
     var Label = project.getOrCreateLabel('Duka Receipt');
+    console.log('sent message' + SMStext);
     project.sendMessage({
         content: SMStext,
         to_number: contact.phone_number,
