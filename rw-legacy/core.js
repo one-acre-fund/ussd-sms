8525270617fe2a1b03a18527701b1809cde2330a
Sabin Sheja <62881592+shejasabin@users.noreply.github.com>
2020-08-05 08:52:21 +0200

    Update core.js
    resolve conflicts

--- rw-legacy/core.js
+++ rw-legacy/core.js
@@ -31,12 +31,8 @@ if(env === 'prod'){
     service.vars['21a_client_data_id'] = project.vars['21a_client_data_id'];
     service.vars.client_enrollment_table_id = project.vars.client_enrollment_data_id;
     service.vars.market_access_table_id = 'DT278868f96626c4b0';
-<<<<<<< HEAD
-    account_splash_menu_name = 'core_enr_splash_menu',
-=======
     service.vars.chicken_table_id = 'DT588706b3a7513443'
     account_splash_menu_name = 'core_enr_splash_menu'
->>>>>>> origin/develop
 }else{
     service.vars.season_clients_table = 'dev_' + project.vars.season_clients_table;
     service.vars.client_enrollment_table = 'dev_' + project.vars.client_enrollment_data;
