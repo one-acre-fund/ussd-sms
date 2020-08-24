7913b696516a47bb61758b3a8e4eb71251eba968
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-08-24 07:47:11 +0200

    moved the beyonic-update-code-offline ;

---
+++ beyonic-update-retry/beyonicUpdateRetry.test.js
@@ -0,0 +1,20 @@
+describe('start', () => {
+    beforeAll(() => {
+        contact.vars.collectionRequest = {
+            phonenumber: '',
+            metadata: { 
+                accountNo: '1234'
+            },
+            amount: '1234',
+            status: '',
+            error_message: '',
+            id: '',
+        };
+        contact.phone_number = '';
+    });
+    beforeEach(() => {
+
+    });
+
+
+});
