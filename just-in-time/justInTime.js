edb8bc553ee133651dad6fb566c39ad5d45c29e0
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-08-25 09:22:48 +0200

    moved a function outside registerHandlers

--- just-in-time/justInTime.js
+++ just-in-time/justInTime.js
@@ -6,19 +6,6 @@ var notifyELK = require('../notifications/elk-notification/elkNotification');
 var translate =  createTranslator(translations, project.vars.lang);
 module.exports = {
     registerHandlers: function (){
-        
-        function onAccountNumberValidated(){
-            var client = JSON.parse(state.vars.topUpClient);
-            var remainingLoan = 0;
-            if(client.BalanceHistory.length > 0){
-                client.latestBalanceHistory = client.BalanceHistory[0];
-                remainingLoan = client.latestBalanceHistory.TotalCredit - client.latestBalanceHistory.TotalRepayment_IncludingOverpayments;
-            }
-            if(remainingLoan > 500 ){
-                var amountToPay = remainingLoan-500;
-                global.sayText(translate('loan_payment_not_satisfied',{'$amount': amountToPay },state.vars.jitLang));
-            }
-        }
         addInputHandler(accountNumberHandler.handlerName, accountNumberHandler.getHandler(onAccountNumberValidated));
     },
 
@@ -32,3 +19,16 @@ module.exports = {
         global.promptDigits(accountNumberHandler.handlerName);
     }
 };
+
+function onAccountNumberValidated(){
+    var client = JSON.parse(state.vars.topUpClient);
+    var remainingLoan = 0;
+    if(client.BalanceHistory.length > 0){
+        client.latestBalanceHistory = client.BalanceHistory[0];
+        remainingLoan = client.latestBalanceHistory.TotalCredit - client.latestBalanceHistory.TotalRepayment_IncludingOverpayments;
+    }
+    if(remainingLoan > 500 ){
+        var amountToPay = remainingLoan-500;
+        global.sayText(translate('loan_payment_not_satisfied',{'$amount': amountToPay },state.vars.jitLang));
+    }
+}
