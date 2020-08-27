0e49e393f1018e5939b2eb2eeb42b61764903b69
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-08-27 21:47:50 +0200

    environment setup

--- update-bundles/updateKenyaTopUpBundles.js
+++ update-bundles/updateKenyaTopUpBundles.js
@@ -1,4 +1,21 @@
+var defaultEnvironment;
+if(service.active){
+    defaultEnvironment = 'prod';
+}else{
+    defaultEnvironment = 'dev';
+}
+
+var env;
+if(service.vars.env === 'prod' || service.vars.env === 'dev'){
+    env = service.vars.env;
+}else{
+    env = defaultEnvironment;
+}
+
+service.vars.server_name = project.vars[env+'_server_name'];
+service.vars.roster_api_key = project.vars[env+'_roster_api_key']; 
 service.vars.inputTableId = 'DT545a7c5683114b75';
+
 var districts = require('./topUpKenyaDistricts');
 var updateBundlesHelperFunctions= require('./updateBundlesHelperFunctions');
 
