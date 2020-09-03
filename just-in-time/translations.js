a132c36ef1e8e2a62f60afecf8aabafabb3226b4
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-09-03 19:23:05 +0200

    added a populate menu helper for bundles and some translations to be used

--- just-in-time/translations.js
+++ just-in-time/translations.js
@@ -7,5 +7,40 @@ module.exports = {
     'loan_payment_not_satisfied': {
         'en-ke': 'You do not qualify for a top up, pay at least $amount to qualify.',
         'sw': 'Bado haujahitimu kuongeza bidhaa, lipa $amount  kuhitimu.'
+    },
+    'bundle_title': {
+        'en-ke': 'Select a product',
+        'sw': 'Chagua product'
+    },
+    'variety_title': {
+        'en-ke': 'Select seed variety',
+        'sw': 'Chagua mbegu'
+    },
+    'order_placed': {
+        'en-ke': 'Order placed\n $orders \n 1) Add product\n 2) Finish ordering',
+        'sw': 'Bidhaa ulizo agiza\n $orders \n 1) Ongeza bidhaa\n 2) Maliza ombi'
+    },
+    'final_order_display': {
+        'en-ke': 'Order placed\n $orders \n1) Confirm order',
+        'sw': 'Bidhaa ulizo agiza\n $orders \n1) kudhibitisha'
+    },
+    'final_message': {
+        'en-ke': 'Thank you for placing your Just in Time Top-up order.',
+        'sw': 'Asante kwa kutuma maombi yako ya Just in Time Top-up.'
+    },
+    'variety_confirmation': {
+        'en-ke': 'Top-up with $bundleName and $inputName\n1) Confirm\n2) Cancel',
+        'sw': '"Umeongeza  $bundleName na $inputName\n1) Thibitisha\n 2) Futa"'
+    },'enrollment_failed': {
+        'en-ke': 'Please try again later. Most people are applying right now!',
+        'sw': 'Tafadhali jaribu tena baada ya mda kidogo. Watu wengi wanatuma maombi kwa sasa!'
+
+    },'prev_page': {
+        'en-ke': '44)Previous page',
+        'sw': '44)Previous page'
+    },
+    'next_page': {
+        'en-ke': '77)Next page',
+        'sw': '77)Next page'
     }
 };
