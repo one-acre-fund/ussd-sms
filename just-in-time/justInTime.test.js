f3fd087a912d3a30e6d0558b97274fd1a669151c
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-09-05 10:29:57 +0200

    adding bundles for kenya

--- just-in-time/justInTime.test.js
+++ just-in-time/justInTime.test.js
@@ -1,17 +1,41 @@
 const accountNumberHandler = require('./account-number-handler/accountNumberHandler');
+const bundleChoiceHandler = require('./bundle-choice-handler/bundleChoiceHandler');
+const addOrderHandler = require('./add-order-handler/addOrderHandler');
+const orderConfirmationHandler = require('./order-confirmation-handler/orderConfirmationHandler');
+const varietyChoiceHandler = require('./variety-choice-handler/varietyChoiceHandler');
+const varietyConfirmationHandler = require('./variety-confirmation-handler/varietyConfirmationHandler');
 const justInTime = require('./justInTime');
 var {client}  = require('../client-enrollment/test-client-data');
 var notifyELK = require('../notifications/elk-notification/elkNotification');
 
 jest.mock('../notifications/elk-notification/elkNotification');
 jest.mock('./account-number-handler/accountNumberHandler');
+jest.mock('./bundle-choice-handler/bundleChoiceHandler');
+jest.mock('./add-order-handler/addOrderHandler');
+jest.mock('./order-confirmation-handler/orderConfirmationHandler');
+jest.mock('./variety-choice-handler/varietyChoiceHandler');
+jest.mock('./variety-confirmation-handler/varietyConfirmationHandler');
+
 
 const mockAccountNumberHandler = jest.fn();
+const mockBundleChoiceHandler = jest.fn();
+const mockAddOrderHandler = jest.fn();
+const mockOrderConfirmationHandler = jest.fn();
+const mockVarietyChoiceHandler = jest.fn();
+const mockVarietyConfirmationHandler = jest.fn();
+
 
 const account = 123456789;
 const country = 'KE';
 const jitLang = 'en-ke';
-
+state.vars.jitLang = 'en-ke';
+state.vars.topUpClient = JSON.stringify(client);
+const phoneNumber = '0786182097';
+var mockRows = [{vars: {'bundleId': '-2009','bundleInputId': '-1709','bundle_name': 'Second possible name bundle','price': '2251','input_name': 'second input'}},{vars: {'bundleId': '-9009','bundleInputId': '-5709','bundle_name': 'third possible name bundle','price': '6251','input_name': 'third input'}},{vars: {'bundleId': '-1009','bundleInputId': '-8709','bundle_name': 'fourth possible name bundle','price': '5251','input_name': 'fourth input'}},{vars: {'bundleId': '-2009','bundleInputId': '-18909','bundle_name': 'Second possible name bundle','price': '2251','input_name': 'second input'}}];
+var mockBundleInputs = [{'bundleId': '-2009','bundleInputId': '-1709','bundleName': 'Second possible name bundle','price': '2251','inputName': 'second input'},{'bundleId': '-9009','bundleInputId': '-5709','bundleName': 'third possible name bundle','price': '6251','inputName': 'third input'},{'bundleId': '-1009','bundleInputId': '-8709','bundleName': 'fourth possible name bundle','price': '5251','inputName': 'fourth input'},{'bundleId': '-2009','bundleInputId': '-18909','bundleName': 'Second possible name bundle','price': '2251','inputName': 'second input'}];
+var mockMaizeRows = [{vars: {'bundleId': '-2009','bundleInputId': '-1709','bundle_name': 'Second possible name bundle','price': '2251','input_name': 'second input'}},{vars: {'bundleId': '-9009','bundleInputId': '-5709','bundle_name': 'third possible name bundle','price': '6251','input_name': 'third input'}}];
+var mockRow = {vars: {'bundleId': '-3009','bundleInputId': '-12109','bundle_name': 'Knapsack Sprayer','price': '2251','input_name': 'Knapsack Sprayer'}};
+var mockBundleInput = [{'bundleId': '-3009','bundleInputId': '-12109','bundleName': 'Knapsack Sprayer','price': '2251','inputName': 'Knapsack Sprayer'}];
 describe('clientRegistration', () => {
 
     it('should have a start function', () => {
@@ -20,17 +44,52 @@ describe('clientRegistration', () => {
     beforeAll(()=>{
         state.vars.jitLang = jitLang;
         state.vars.topUpClient = JSON.stringify(client);
+
     });
     beforeEach(() => {
         accountNumberHandler.getHandler.mockReturnValue(mockAccountNumberHandler);
+        bundleChoiceHandler.getHandler.mockReturnValue(mockBundleChoiceHandler);
+        addOrderHandler.getHandler.mockReturnValue(mockAddOrderHandler);
+        orderConfirmationHandler.getHandler.mockReturnValue(mockOrderConfirmationHandler);
+        varietyChoiceHandler.getHandler.mockReturnValue(mockVarietyChoiceHandler);
+        varietyConfirmationHandler.getHandler.mockReturnValue(mockVarietyConfirmationHandler);
 
     });
     it('should add the account number handler to input handlers', () => {
         justInTime.registerHandlers();
         expect(addInputHandler).toHaveBeenCalledWith(accountNumberHandler.handlerName, accountNumberHandler.getHandler());            
     });
+    it('should add the bundle Choice Handler  to input handlers', () => {
+        justInTime.registerHandlers();
+        expect(addInputHandler).toHaveBeenCalledWith(bundleChoiceHandler.handlerName, bundleChoiceHandler.getHandler());            
+    });
+    it('should add the add order handler to input handlers', () => {
+        justInTime.registerHandlers();
+        expect(addInputHandler).toHaveBeenCalledWith(addOrderHandler.handlerName, addOrderHandler.getHandler());            
+    });
+    it('should add the order  confirmation handler to input handlers', () => {
+        justInTime.registerHandlers();
+        expect(addInputHandler).toHaveBeenCalledWith(orderConfirmationHandler.handlerName, orderConfirmationHandler.getHandler());            
+    });
+    it('should add the variety choice handler to input handlers', () => {
+        justInTime.registerHandlers();
+        expect(addInputHandler).toHaveBeenCalledWith(varietyChoiceHandler.handlerName, varietyChoiceHandler.getHandler());            
+    });
+    it('should add the variety confirmation handler to input handlers', () => {
+        justInTime.registerHandlers();
+        expect(addInputHandler).toHaveBeenCalledWith(varietyConfirmationHandler.handlerName, varietyConfirmationHandler.getHandler());            
+    });
     describe('Account number handler success callback', () => {
         var callback;
+        const mockCursor = { next: jest.fn(), 
+            hasNext: jest.fn()
+        };
+        const mockTable = { queryRows: jest.fn() };
+        mockTable.queryRows.mockReturnValue(mockCursor);
+        
+        project.initDataTableById.mockReturnValue(mockTable);
+        state.vars.orders =  ' ';
+        state.vars.jitLang ='en-ke';
         beforeEach(() => {
             justInTime.registerHandlers();
             callback = accountNumberHandler.getHandler.mock.calls[0][0];                
@@ -39,6 +98,57 @@ describe('clientRegistration', () => {
             callback();
             expect(sayText).not.toHaveBeenCalledWith(expect.stringContaining('You do not qualify for a top up,'));
         });
+        it('should display  the bundles if the prepayment condition is ssatified',()=>{
+            mockCursor.hasNext.mockReturnValueOnce(true);
+            mockCursor.next.mockReturnValueOnce(mockRow);
+            callback();
+            expect(sayText).not.toHaveBeenCalledWith(expect.stringContaining('You do not qualify for a top up,'));
+            expect(state.vars.main_menu).toEqual(`Select a product\n1) ${mockRow.vars.bundle_name}`+
+            ` ${mockRow.vars.price}`+
+            '\n');
+            //expect(state.vars.bundles).toEqual(mockRow.vars);
+        });
+        it('should display a the bundles with next and previous options if the prepayment condition is satified and there is a lot of bundles',()=>{
+    
+            mockCursor.hasNext.mockReturnValueOnce(true).mockReturnValueOnce(true).mockReturnValueOnce(true).mockReturnValueOnce(true);
+            mockCursor.next.mockReturnValueOnce(mockRow).mockReturnValueOnce(mockRows[0]).mockReturnValueOnce(mockRows[1]).mockReturnValueOnce(mockRows[2]);
+            callback();
+            expect(sayText).not.toHaveBeenCalledWith(expect.stringContaining('You do not qualify for a top up,'));
+            expect(state.vars.main_menu).toEqual(`Select a product\n1) ${mockRow.vars.bundle_name}`+
+            ` ${mockRow.vars.price}`+
+            `\n2) ${mockRows[0].vars.bundle_name}`+
+            ` ${mockRows[0].vars.price}`+
+            `\n3) ${mockRows[1].vars.bundle_name}`+
+            ` ${mockRows[1].vars.price}`+
+            '\n77)Next page');
+        });
+        it('should display only unique bundles(if two bundle inputs in the same bundle are found) if the prepayment condition is satified ', () => {
+
+            mockCursor.hasNext.mockReturnValueOnce(true).mockReturnValueOnce(true).mockReturnValueOnce(true);
+            mockCursor.next.mockReturnValueOnce(mockRow).mockReturnValueOnce(mockRows[0]).mockReturnValueOnce(mockRows[3]);
+            callback();
+            expect(sayText).not.toHaveBeenCalledWith(expect.stringContaining('You do not qualify for a top up,'));
+            expect(state.vars.main_menu).toEqual(`Select a product\n1) ${mockRow.vars.bundle_name}`+
+            ` ${mockRow.vars.price}`+
+            `\n2) ${mockRows[0].vars.bundle_name}`+
+            ` ${mockRows[0].vars.price}`+
+            '\n');
+        });
+        
+        it('should remove maize bundles from the displayed bundles if the prepayment condition is satified and the client already choosed a maize bundle',()=>{
+            
+            mockCursor.hasNext.mockReturnValueOnce(true).mockReturnValueOnce(true).mockReturnValueOnce(false).mockReturnValueOnce(true);
+            mockCursor.next.mockReturnValueOnce(mockRow).mockReturnValueOnce(mockRows[0]).mockReturnValueOnce(mockRows[1]).mockReturnValueOnce(mockMaizeRows[0]).mockReturnValueOnce(mockMaizeRows[1]);
+            state.vars.orders = JSON.stringify([mockMaizeRows[0]]);
+            callback();
+            expect(sayText).not.toHaveBeenCalledWith(expect.stringContaining('You do not qualify for a top up,'));
+            expect(state.vars.main_menu).toEqual(`Select a product\n1) ${mockRow.vars.bundle_name}`+
+            ` ${mockRow.vars.price}`+
+            `\n2) ${mockRows[0].vars.bundle_name}`+
+            ` ${mockRows[0].vars.price}`+
+            '\n');
+        });
+        
         it('should display a message saying that the prepayment condition is not satified if the remaining loan is greater than 500', () => {
             client.BalanceHistory[0].TotalCredit = 5000;
             client.BalanceHistory[0].TotalRepayment_IncludingOverpayments = 0;
@@ -47,6 +157,109 @@ describe('clientRegistration', () => {
             callback();
             expect(sayText).toHaveBeenCalledWith(`You do not qualify for a top up, pay at least ${amount}`+
             ' to qualify.');
+        });
+        
+    });
+
+    describe('bundle Choice Handler successfull callback',()=>{
+        var callback;
+        beforeEach(() => {
+            justInTime.registerHandlers();
+            callback = bundleChoiceHandler.getHandler.mock.calls[0][0];    
+            state.vars.bundleInputs =  JSON.stringify(mockBundleInputs);          
+        });
+
+        it('should display the varieties for the bundle choosed, if the the bundle has multiple varieties',()=>{
+            console.log('pppppppppppppppppppppppppppppppp'+mockBundleInputs[0].bundleId);
+            callback(mockBundleInputs[0].bundleId);
+            expect(sayText).toHaveBeenCalledWith(`Select seed variety\n1) ${mockBundleInputs[0].inputName}`+
+            `\n2) ${mockBundleInputs[3].inputName}`+
+            '\n');
+        });
+        it('should display the order placed by the user and prompt to add order or finalize order',()=>{
+            state.vars.bundleInputs =  JSON.stringify(mockBundleInput);  
+            state.vars.orders = ' ';
+            var orderMessage = mockBundleInput[0].bundleName + ' ' + mockBundleInput[0].price + '\n';
+            console.log('pppppppppppppppppppppppppppppppp'+mockBundleInputs[0].bundleId);
+            callback(mockBundleInput[0].bundleId);
+            expect(sayText).toHaveBeenCalledWith(`Order placed\n ${orderMessage}`+
+            ' \n 1) Add product\n 2) Finish ordering');
+            expect(promptDigits).toHaveBeenCalledWith(addOrderHandler.handlerName);
+        });
+
+    });
+
+    describe('order Confirmation Handler successfull callback',()=>{
+        var callback;
+        beforeAll(()=>{
+            state.vars.orders = JSON.stringify([mockMaizeRows[0]]);
+        });
+        beforeEach(() => {
+            justInTime.registerHandlers();
+            callback = orderConfirmationHandler.getHandler.mock.calls[0][0];           
+        });
+
+        it('should display a message confirming the order is complete if the order issaved in roster',()=>{
+            httpClient.request.mockReturnValue({status: 201});
+            contact.phone_number = phoneNumber;
+            var message = 'Thank you for placing your Just in Time Top-up order.';
+            callback();
+            expect(sayText).toHaveBeenCalledWith('Thank you for placing your Just in Time Top-up order.');
+            expect(project.sendMessage).toHaveBeenCalledWith({content: message, to_number: phoneNumber});
+        });
+        it('should display a message asking the user to try again later the order is not saved in roster',()=>{
+            httpClient.request.mockReturnValue({status: 500});
+            callback();
+            expect(sayText).toHaveBeenCalledWith('Please try again later. Most people are applying right now!');
+        });
+    });
+    describe('variety Choice Handler success callback',()=>{
+        var callback;
+        beforeEach(() => {
+            justInTime.registerHandlers();
+            callback = varietyChoiceHandler.getHandler.mock.calls[0][0]; 
+            state.vars.bundleInputs =  JSON.stringify(mockBundleInputs);           
+        });
+
+        it('should display a confirm variety message and prompt for input',()=>{
+            callback(mockBundleInputs[0]);
+            expect(sayText).toHaveBeenCalledWith(`Top-up with ${mockBundleInputs[0].bundleName}`+
+            ` and ${mockBundleInputs[0].inputName}`+
+            '\n1) Confirm\n2) Cancel');
+            expect(promptDigits).toHaveBeenCalledWith(varietyConfirmationHandler.handlerName);
+        });
+ 
+    });
+
+    describe('variety Confirmation Handler successfull callback',()=>{
+        var callback;
+        beforeEach(() => {
+            justInTime.registerHandlers();
+            state.vars.orders = ' ';
+            callback = varietyConfirmationHandler.getHandler.mock.calls[0][0];    
+            state.vars.bundleInputs =  JSON.stringify(mockBundleInputs);        
+        });
+        it('should show the order placed',()=>{
+            callback(mockBundleInputs[3].bundleId,true,mockBundleInputs[3].bundleInputId);
+            var orderMessage = mockBundleInputs[3].bundleName + ' ' + mockBundleInputs[3].price + '\n';
+            expect(sayText).toHaveBeenCalledWith(`Order placed\n ${orderMessage}`+
+            ' \n 1) Add product\n 2) Finish ordering');
+
+        });
+
+    });
+    describe('add Order Handler successfull callback',()=>{
+        var callback;
+        beforeEach(() => {
+            justInTime.registerHandlers();
+            state.vars.orders = JSON.stringify(mockBundleInput);
+            callback = addOrderHandler.getHandler.mock.calls[0][0];          
+        });
+        it('should show the order placed',()=>{
+            callback();
+            var orderMessage = mockBundleInput[0].bundleName + ' ' + mockBundleInput[0].price + '\n';
+            expect(sayText).toHaveBeenCalledWith(`Order placed\n ${orderMessage}`+
+            ' \n1) Confirm order');
         });
     });
 
---
+++ update-bundles/updateAllBundle.js
@@ -0,0 +1,89 @@
+var defaultEnvironment;
+if(service.active){
+    defaultEnvironment = 'prod';
+}else{
+    defaultEnvironment = 'dev';
+}
+
+var env;
+if(service.vars.env === 'prod' || service.vars.env === 'dev'){
+    env = service.vars.env;
+}else{
+    env = defaultEnvironment;
+}
+
+service.vars.server_name = project.vars[env+'_server_name'];
+service.vars.roster_api_key = project.vars[env+'_roster_api_key']; 
+service.vars.inputTableId = 'DTe0cd7f4439c55c3f';
+
+var districts = require('./KenyaAllDistricts');
+var updateBundlesHelperFunctions= require('./updateBundlesHelperFunctions');
+
+global.main= function(){
+    state.vars.counter = 0;
+    sayText('press any key to start');
+    promptDigits('update_bundles', {
+        'submitOnHash': false,
+        'maxDigits': 1,
+        'timeout': 180
+    });
+
+};
+var districtBundles;
+var inputHanlder = function(input){
+    if(state.vars.counter < districts.length){
+        var currentDistricts = districts.slice(state.vars.counter, (state.vars.counter)+3);
+        currentDistricts.forEach(function(district){
+            console.log('***********District:'+ district+ 'started******************');
+            districtBundles = updateBundlesHelperFunctions.fetchDistrictBundles(district);
+            updateBundlesHelperFunctions.processBundles(districtBundles, district);
+            console.log('#################################District: '+district+'finished#######################');
+            console.log(input);
+        });
+        console.log(state.vars.counter);
+        state.vars.counter = (state.vars.counter) + 3;
+        console.log(state.vars.counter);
+        sayText('press any key to continue');
+        promptDigits('update_bundles_two', {
+            'submitOnHash': false,
+            'maxDigits': 1,
+            'timeout': 180
+        });
+    }
+    else{
+        sayText('Updating finished !');
+    }
+};
+var inputHanlderTwo = function(input){
+    if(state.vars.counter < districts.length){
+        var currentDistricts = districts.slice(state.vars.counter, (state.vars.counter)+3);
+        currentDistricts.forEach(function(district){
+            console.log('***********District:'+ district+ 'started******************');
+            districtBundles = updateBundlesHelperFunctions.fetchDistrictBundles(district);
+            updateBundlesHelperFunctions.processBundles(districtBundles, district);
+            console.log('#################################District: '+district+'finished#######################');
+            console.log(input);
+        });
+        console.log(state.vars.counter);
+        state.vars.counter = (state.vars.counter) + 3;
+        console.log(state.vars.counter);
+        sayText('press any key to continue');
+        promptDigits('update_bundles', {
+            'submitOnHash': false,
+            'maxDigits': 1,
+            'timeout': 180
+        });
+    }
+    else{
+        sayText('Updating finished !');
+    }
+};
+addInputHandler('update_bundles', inputHanlder);
+addInputHandler('update_bundles_two', inputHanlderTwo);
+// var districtBundles;
+// districts.forEach(function(district){
+//     console.log('***********District:'+ district+ 'started******************');
+//     districtBundles = updateBundlesHelperFunctions.fetchDistrictBundles(district);
+//     updateBundlesHelperFunctions.processBundles(districtBundles, district);
+//     console.log('#################################District: '+district+'finished#######################');
+// });
