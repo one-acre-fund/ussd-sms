6052499188278232baac519b70868c45fa9cae7c
BAHATI Robben <bahatiroben@gmail.com>
2020-09-08 08:01:38 +0200

    sending the messages for batch 1

---
+++ soil-fertility-trainings/soilFertilityTraining.js
@@ -0,0 +1,33 @@
+var defaultEnvironment;
+if(service.active){
+    defaultEnvironment = 'prod';
+}else{
+    defaultEnvironment = 'dev';
+}
+
+var env;
+if(service.vars.env === 'prod' || service.vars.env === 'dev'){
+    env = service.vars.env;
+}else{
+    env = defaultEnvironment;
+}
+
+console.log(env);
+
+var translations = require('./translations/index');
+var translator = require('../utils/translator/translator');
+// var addResponseHandlers = require('./responseHandlers/addResponseHandlers');
+
+var lang = contact.vars.lang;
+var getMessage = translator(translations, lang);
+global.main = function() {
+    project.sendMulti({
+        messages: [
+            {content: getMessage('sms-1.1', {}, lang), to_number: contact.phone_number}, 
+            {content: getMessage('sms-1.2', {}, lang), to_number: contact.phone_number}], 
+        message_type: 'text'
+    });
+    waitForResponse('sms_1_response');
+};
+
+// addResponseHandlers(lang);
