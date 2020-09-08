9f37af7f0764cbedfcd42ba1e69abe784c5b2f64
BAHATI Robben <bahatiroben@gmail.com>
2020-09-08 14:32:07 +0200

    replacing the trigger function from contact to call

--- ke-legacy/main.js
+++ ke-legacy/main.js
@@ -249,6 +249,7 @@ var TriggerTraining = function (ServiceID){
     }
 };
 
+
 var LocationNotKnown = function (Location){
     if (Location == '#'|| Location == '0'){
         LocationNotKnownText();
@@ -1778,7 +1779,7 @@ addInputHandler('MainMenu', function(SplashMenu){
         } else {
             lang = 'sw';
         }
-        maizeRecommendation(lang, TriggerTraining, project.vars.maize_recommendation_service_id);
+        maizeRecommendation(lang, project.vars.maize_recommendation_service_id)
     }
     else{
         var arrayLength = client.BalanceHistory.length;
--- maize-recommendation/triggerService.js
+++ maize-recommendation/triggerService.js
@@ -1,5 +1,19 @@
 var translations = require('./translations/index');
 var translator = require('../utils/translator/translator');
+var slacLogger = require('../slack-logger/index');
+
+function trigger(ServiceID) {
+    try{
+        var service = project.initServiceById(ServiceID);
+        service.invoke({
+            context: 'call', 
+        });
+    }
+    catch(err){
+        slacLogger.log('Error triggering service: ' + ServiceID + JSON.stringify({error: err}));
+    }
+}
+
 
 /**
  * triggers the messaging service for maize recommendation
@@ -7,7 +21,7 @@ var translator = require('../utils/translator/translator');
  * @param {Function} trigger function to trigger the messaging service
  * @param {String} serviceId the service to be triggered
  */
-module.exports = function(lang, trigger, serviceId) {
+module.exports = function(lang, serviceId) {
     var getMessage = translator(translations, lang);
     var message = getMessage('message_notification', {}, lang);
     sayText(message);
