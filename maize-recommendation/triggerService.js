c8f5832e4dddb50f53cbc792815dd110b9ae289a
BAHATI Robben <bahatiroben@gmail.com>
2020-09-08 15:13:30 +0200

    fixing the unit tests and removing the unused variable

--- maize-recommendation/triggerService.js
+++ maize-recommendation/triggerService.js
@@ -3,13 +3,17 @@ var translator = require('../utils/translator/translator');
 var slacLogger = require('../slack-logger/index');
 
 function trigger(ServiceID) {
+    console.log('reaching 1' + ServiceID);
     try{
+        console.log('reaching 2' + ServiceID);
         var service = project.initServiceById(ServiceID);
         service.invoke({
             context: 'call', 
         });
+        console.log('reaching 3' + ServiceID);
     }
     catch(err){
+        console.log('reaching 3' + ServiceID + JSON.stringify({error: err}));
         slacLogger.log('Error triggering service: ' + ServiceID + JSON.stringify({error: err}));
     }
 }
@@ -18,7 +22,6 @@ function trigger(ServiceID) {
 /**
  * triggers the messaging service for maize recommendation
  * @param {String} lang language to be used
- * @param {Function} trigger function to trigger the messaging service
  * @param {String} serviceId the service to be triggered
  */
 module.exports = function(lang, serviceId) {
