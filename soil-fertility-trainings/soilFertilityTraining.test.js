014587b7132424b9f824790fa2e4c415f484a52e
BAHATI Robben <bahatiroben@gmail.com>
2020-09-09 08:59:01 +0200

    removing the main function

--- soil-fertility-trainings/soilFertilityTraining.test.js
+++ soil-fertility-trainings/soilFertilityTraining.test.js
@@ -13,7 +13,6 @@ describe('soil fertility training', () => {
         jest.mock('./responseHandlers/addResponseHandlers');
         contact.phone_number = '0555345';
         require('./soilFertilityTraining');
-        global.main();
         expect(project.sendMulti).toHaveBeenCalledWith({'message_type': 'text', 'messages': [
             {'content': 'Today we are going learn how to get the best harvest from your soils. Healthy soils give great harvest! Soil is gold!', 'to_number': '0555345'}, 
             {'content': 'Each type of crop needs a different mix of nutrients for great harvests. Healthy soils provide a complete range of nutrients to plants.', 'to_number': '0555345'}
