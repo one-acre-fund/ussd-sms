693e2af45046ae0308631b81df3ac4911faca7a5
BAHATI Robben <bahatiroben@gmail.com>
2020-09-09 14:12:19 +0200

    adding the translations file for duka client main menu

---
+++ duka-client/translations/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'service_title': {
+        'en-ke': 'Select service',
+        'sw': 'Chagua huduma'
+    },
+    'register_client': {
+        'en-ke': '$label) Register Client',
+        'sw': '$label) Kusajili Mteja'
+    }
+};
