dcf5677e1a9ef431fb44beabace27c2d31e9b953
BAHATI Robben <bahatiroben@gmail.com>
2020-09-10 09:28:44 +0200

    adding the translations file for duka client registration (no swahili yet)

---
+++ duka-client/registration/translations/index.js
@@ -0,0 +1,62 @@
+module.exports = {
+    'account_number': {
+        'en-ke': 'Please reply with the account number of the farmer\n"0" for new client.',
+        'sw': '"Tafadhali jibu na nambari ya akaunti ya mkulima\n"0" kwa mteja mpya."'
+    },
+    'non_duka_account': {
+        'en-ke': 'You\'ve entered an account number for a non-duka account. Enter your duka account number or 0 to register as a duka client.',
+        'sw': ''
+    },
+    'already_registered_enter': {
+        'en-ke': 'You\'re already registered for a duka district. Please enter the client\'s invoice ID.',
+        'sw': ''
+    },
+    'outstanding_balance': {
+        'en-ke': 'You have a Duka credit account with an outstanding credit balance of $balance. Please complete your loan in order to take another one.',
+        'sw': ''
+    },
+    'confirm_or_try': {
+        'en-ke': '1) To confirm\n2) To try again.',
+        'sw': ''
+    },
+    'enter_phone': {
+        'en-ke': 'Enter client Phone number',
+        'sw': ''
+    },
+    'enter_first_name': {
+        'en-ke': 'Please reply with the first name of the client.',
+        'sw': ''
+    },
+    'enter_second_name': {
+        'en-ke': 'Please reply with the second name of the client.',
+        'sw': ''
+    },
+    'enter_invoice_id': {
+        'en-ke': 'Please reply with the client\'s Erply Invoice ID',
+        'sw': ''
+    },
+    'thanks_for_registering': {
+        'en-ke': 'Thank you for registering with OAF. Your Account Number is $account_number. The Duka team will help you complete your loan.',
+        'sw': ''
+    },
+    'confirm_or_cancel': {
+        'en-ke': '1) Confirm\n2) Cancel',
+        'sw': ''
+    },
+    'invoice_id_confirm': {
+        'en-ke': 'You entered invoice ID $invoice_id\n',
+        'sw': ''
+    },
+    'account_number_confirm': {
+        'en-ke': 'You entered $national_id ID\n',
+        'sw': ''
+    },
+    'phone_number_confirm': {
+        'en-ke': 'You entered phone number $phone_number\n',
+        'sw': ''
+    },
+    'first_sencond_name_confirm': {
+        'en-ke': 'Enroll client $first_name $second_name',
+        'sw': ''
+    } 
+};
