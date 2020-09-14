d4c163cea376841c4e46a8739b6555f03b527b6c
BAHATI Robben <bahatiroben@gmail.com>
2020-09-14 10:08:52 +0200

    adding a translation file for some rw-pshops user journey

---
+++ agrodealer-locator/translations/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    'districts_title': {
+        'en': 'To find a One Acre Fund Agrodealer Partner near you, select District\n$Menu',
+        'ki': 'Kubona umufatanya bikorwa ukwegereye, hitamo Akarere\n$Menu'
+    },
+    'list': {
+        'en': '$label) $option\n',
+        'ki': '$label) $option\n'
+    },
+    'sms_to_farmer': {
+        'en': '$Agrodealer_name is you nearest One Acre Fund Agrodealer partner shop. They are located in $Cell and offer credit and training on $days_of_week. For more information you can contact your Shop Officer officer_name on officer_phone or your Village Leader.',
+        'ki': ''
+    },
+    'sms_to_officer': {
+        'en': 'There is a potential client with phonenumber client_phone. Please call them back to follow up. Thanks',
+        'ki': ''
+    },
+    'invalid_district': {
+        'en': 'We do not currently have an agrodealer partner shop located in your District',
+        'ki': ''
+    },
+    'sectors_title': {
+        'en': '$district: Please select the Sector you live in.\n$Menu',
+        'ki': '$district: Hitamo umurege utuyemo\n$Menu'
+    }
+};
