c8e5553e58657f33c5381c7889a6f24ab25273b9
BAHATI Robben <bahatiroben@gmail.com>
2020-09-15 08:05:50 +0200

    adding the districts input handler test to test the call to the sectors generator and connecting the the sectors input handler file

--- agrodealer-locator/inputHandlers.js/districtInputHandler.js
+++ agrodealer-locator/inputHandlers.js/districtInputHandler.js
@@ -1,5 +1,6 @@
 var translations = require('../translations/index');
 var translator = require('../../utils/translator/translator');
+var sectorsInputHandler = require('./sectorInputHandler');
 
 var handlerName = 'pshops_locator_district_handler';
 var districts = {
@@ -17,12 +18,12 @@ module.exports = {
      * @param {Function} displayDistricts function that displays districts screen
      * @param {Function} displaySectors function to display the sectors screen
      */
-    getHandler: function(lang, displayDistricts, getSectors) {
+    getHandler: function(lang, displayDistricts, getSectors, agrodealers_address_table) {
         return function(input) {
             var getMessage = translator(translations, lang);
             var district = districts[input.toString().trim()];
             if(district) {
-                var sectorsObject = getSectors(district);
+                var sectorsObject = getSectors(district, agrodealers_address_table);
                 var screens = sectorsObject.screens;
                 var sectorsList = sectorsObject.list;
                 state.vars.sectors_list = JSON.stringify(sectorsList);
@@ -30,7 +31,7 @@ module.exports = {
                 state.vars.sectors_screens = JSON.stringify(screens);
                 state.vars.selected_district = district;
                 sayText(screens[state.vars.current_sectors_screen]);
-                promptDigits('pshops_locator_sector_handler');
+                promptDigits(sectorsInputHandler.handlerName);
             } else if(input == 5) {
                 sayText(getMessage('invalid_district', {}, lang));
                 stopRules();
