dc34704efc8a8e8585d0b328c61ce2a9827714d2
BAHATI Robben <bahatiroben@gmail.com>
2020-09-16 12:19:39 +0200

    adding the registration of a returning client to a duka ditrict client

---
+++ duka-client/registration/inputHandlers/accountNumberInputHandler.js
@@ -0,0 +1,128 @@
+var translations = require('../translations/index');
+var translator = require('../../../utils/translator/translator');
+var getClient = require('../../../shared/rosterApi/getClient');
+var getPhoneNumbers = require('../../../shared/rosterApi/getPhoneNumber');
+var registerClient = require('../../../shared/rosterApi/registerClient');
+
+var handlerName = 'dukaClientAccNumOrnewClient';
+
+function isADukaClient(district, credit_officers_table) {
+    var table = project.getOrCreateDataTable(credit_officers_table);
+    var cursor = table.queryRows({
+        vars: {
+            district: district
+        }
+    });
+    
+    if(cursor.hasNext()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getActivePhoneNumber(accNum, country) {
+    var phoneNumbers = getPhoneNumbers(accNum, country);
+    var activePhoneNumber = phoneNumbers.filter(function(record) {
+        return record.IsInactive == false;
+    });
+    return activePhoneNumber[0] && activePhoneNumber[0].PhoneNumber;
+}
+
+/**
+ * Checks whether the user has outstanding creedit in any season
+ * @param {[object]} BalanceHistory 
+ */
+function OutstandingCredit(BalanceHistory){
+    var withBalance = BalanceHistory.filter(function(record) {
+        return record.Balance > 0;
+    });
+
+    var outStandingCredit = 0;
+    withBalance.forEach(function(record){
+        outStandingCredit = outStandingCredit + record.Balance;
+    });
+    return outStandingCredit;
+}
+
+module.exports = {
+    handlerName: handlerName,
+    getHandler: function(lang, credit_officers_table, credit_officer_details) {
+        return function(input) {
+            var getMessage = translator(translations, lang);
+            if(input == 0) {
+                if(state.vars.dcr_duka_client) {
+                    var dcr_duka_client = JSON.parse(state.vars.dcr_duka_client);
+                    var registeredClient = registerClient(dcr_duka_client);
+                    if(registeredClient) {
+                        global.sayText(getMessage('enter_invoice_id', {}, lang));
+                        global.promptDigits('dcr_invoice_id', {
+                            submitOnHash: false
+                        });
+                        return;
+                    } else {
+                        // reprompt
+                        var nonDukaAccountMessagereprompt = getMessage('non_duka_account', {}, lang);
+                        global.sayText(nonDukaAccountMessagereprompt);
+                        state.vars.dcr_duka_client = JSON.stringify(dukaClient);
+                        global.promptDigits(handlerName, {
+                            submitOnHash: false,
+                        });
+                        return;
+                    }
+                }
+                var message = getMessage('enter_national_id');
+                global.sayText(message);
+                global.promptDigits('duka_client_nid', {
+                    submitOnHash: false,
+                });
+            } else {
+                var accountNumber = input.trim();
+                var country = 'kenya';
+                var client = getClient(accountNumber, country);
+                if(client) {
+                    // client has an account registered in roster
+                    var phone_number = getActivePhoneNumber(accountNumber, country);
+                    client.PhoneNumber = phone_number; // keep the session phone at the moment
+                    var district = client.DistrictName;
+                    if(isADukaClient(district, credit_officers_table)){
+                        var outStandingCredit = OutstandingCredit(client.BalanceHistory);
+                        if(outStandingCredit > 0) {
+                            // if has an out standing credit, ask them to pay the remaining amount
+                            global.sayText(getMessage('outstanding_balance', {'$balance': outStandingCredit}, lang));
+                            global.stopRules();
+                        } else {
+                            state.vars.account_number = accountNumber;
+                            state.vars.phone_number = client.PhoneNumber;
+                            // has paid everything
+                            global.sayText(getMessage('enter_invoice_id', {}, lang));
+                            global.promptDigits('dcr_invoice_id', {
+                                submitOnHash: false
+                            });
+                        }
+                    } else {
+                        var nonDukaAccountMessage = getMessage('non_duka_account', {}, lang);
+                        var dukaClient = {
+                            firstName: client.FirstName,
+                            lastName: client.LastName,
+                            phoneNumber: client.PhoneNumber,
+                            siteId: credit_officer_details.site_id,
+                            districtId: credit_officer_details.district_id,
+                            nationalIdNumber: 'DUKA-' + client.NationalId
+                        };
+                        global.sayText(nonDukaAccountMessage);
+                        state.vars.dcr_duka_client = JSON.stringify(dukaClient);
+                        global.promptDigits(handlerName, {
+                            submitOnHash: false,
+                        });
+                    }
+                } else {
+                    global.sayText(getMessage('account_number'));
+                    global.promptDigits(handlerName, {
+                        submitOnHash: false
+                    });
+                }
+            }
+        };
+    }
+};
