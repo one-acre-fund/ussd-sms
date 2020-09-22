a81b346237d1d489809d1459c8a317f109a4edd4
BAHATI Robben <bahatiroben@gmail.com>
2020-09-22 10:47:10 +0200

    adding the confirmation on invoice id input handler

---
+++ duka-client/registration/inputHandlers/confirmInvoiceIdInputHandler.js
@@ -0,0 +1,48 @@
+var translations = require('../translations/index');
+var translator = require('../../../utils/translator/translator');
+var confirmFirstSecondName = require('./confirmFirstSecondNameInputHandler');
+
+
+var handlerName = 'duka_client_registration_invoice_id_confirm';
+
+module.exports = {
+    handlerName: handlerName,
+    getHandler: function(lang, duka_clients_table) {
+        return function(input) {
+            var getMessage = translator(translations, lang);
+            if(input == 1) {
+                if(state.vars.duka_client_first_name) {
+                    var confirmNamesMessageTitle = getMessage('first_sencond_name_confirm', {
+                        '$first_name': state.vars.duka_client_first_name,
+                        '$second_name': state.vars.duka_client_second_name
+                    }, lang);
+        
+                    var confirmNamesMessage = confirmNamesMessageTitle + getMessage('confirm_or_try', {}, lang);
+                    global.sayText(confirmNamesMessage);
+                    global.promptDigits(confirmFirstSecondName.handlerName);
+                } else {
+                    var clientsTable = project.getOrCreateDataTable(duka_clients_table);
+                    var row = clientsTable.createRow({
+                        vars: {
+                            'invoice_id': state.vars.duka_client_invoice_id,
+                            'account_number': state.vars.account_number,
+                            'phone_number': state.vars.phone_number
+                        }
+                    });
+                    row.save();
+                    global.sayText(getMessage('successfull_transaction', {}, lang));
+                    global.stopRules();
+                }
+            } else if(input == 2) {
+                var invoiceIdInputHandler = require('./invoiceIdInputHandler');
+                global.sayText(getMessage('enter_invoice_id', {}, lang));
+                global.promptDigits(invoiceIdInputHandler.handlerName);
+            } else {
+                var promptInvoiceIdConfirmTitle = getMessage('invoice_id_confirm', {'$invoice_id': state.vars.duka_client_invoice_id}, lang);
+                var confirmInvoiceMessace = promptInvoiceIdConfirmTitle + getMessage('confirm_or_try', {}, lang);
+                global.sayText(confirmInvoiceMessace);
+                global.promptDigits(handlerName);
+            }
+        };
+    }
+};
---
+++ duka-client/registration/inputHandlers/confirmInvoiceIdInputHandler.test.js
@@ -0,0 +1,67 @@
+const invoiceIdInputHandler = require('./invoiceIdInputHandler');
+const confirmFirstSecondName = require('./confirmFirstSecondNameInputHandler');
+const confirmInvoiceInputHandler = require('./confirmInvoiceIdInputHandler');
+
+describe.each(['en-ke', 'sw'])('confirm invoice input handler using (%s)', (lang) => {
+    beforeEach(() => {
+        state.vars = {};
+    });
+    it('should reprompt for invoice confirmation when a user enters an invalid option --' + lang, () => {
+        const handler = confirmInvoiceInputHandler.getHandler(lang);
+        state.vars.duka_client_invoice_id = 'DTX009S';
+        handler('00');
+        const message = {
+            'en-ke': 'You entered invoice ID DTX009S\n' +
+            '1) To confirm\n' +
+            '2) To try again.',
+            'sw': 'Uliingiza nambari DTX009S ya invoice. Ingiza\n' +
+            '1) Kudhibitisha\n' +
+            '2) Kujaribu tena.'
+        };
+        expect(sayText).toHaveBeenCalledWith(message[lang]);
+        expect(promptDigits).toHaveBeenCalledWith(confirmInvoiceInputHandler.handlerName);
+    });
+
+    it('should reprompt for invoice id when the user chooses 2 --' + lang, () => {
+        const handler = confirmInvoiceInputHandler.getHandler(lang);
+        handler(2);
+        const message = {
+            'en-ke': 'Please reply with the client\'s Erply Invoice ID',
+            'sw': 'Tafadhali jibu na Kitambulisho cha mteja cha Erply Invoice'
+        };
+        expect(sayText).toHaveBeenCalledWith(message[lang]);
+        expect(promptDigits).toHaveBeenCalledWith(invoiceIdInputHandler.handlerName);
+    });
+
+    it('should prompt for enrollment confirmation once the user chooses 1 and is a new client --' + lang, () => {
+        const handler = confirmInvoiceInputHandler.getHandler(lang);
+        state.vars.duka_client_first_name = 'Jamie';
+        state.vars.duka_client_second_name = 'Fox';
+        handler(1);
+        const message = {
+            'en-ke': 'Enroll client Jamie Fox\n' +
+            '1) To confirm\n' +
+            '2) To try again.',
+            'sw': 'Sajili mteja Jamie Fox\n' +
+            '1) Kudhibitisha\n' +
+            '2) Kujaribu tena.'
+        };
+        expect(sayText).toHaveBeenCalledWith(message[lang]);
+        expect(promptDigits).toHaveBeenCalledWith(confirmFirstSecondName.handlerName);
+    });
+
+    it('should save the user details once the user chooses 1 and is an existing client --' + lang, () => {
+        const handler = confirmInvoiceInputHandler.getHandler(lang);
+        const mockRow = {save: jest.fn()};
+        const mockTable = {createRow: jest.fn(() => mockRow)};
+        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockTable);
+        handler(1);
+        const message = {
+            'en-ke': 'You have successfully recorded your transaction.',
+            'sw': 'Umekamilisha mafanikio ya ununuzi wako.'
+        };
+        expect(mockRow.save).toHaveBeenCalled();
+        expect(sayText).toHaveBeenCalledWith(message[lang]);
+        expect(stopRules).toHaveBeenCalled();
+    });
+});
