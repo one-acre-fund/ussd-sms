59f9149c8066bb0d7731d4b7f2c7a8480f445d08
BAHATI Robben <bahatiroben@gmail.com>
2020-09-23 07:37:33 +0200

    adding the credit officer details to the duka client entry file

--- duka-client/dukaClient.js
+++ duka-client/dukaClient.js
@@ -3,14 +3,15 @@ var translator = require('../utils/translator/translator');
 var mainMenuInputHandler = require('./inputHandlers/mainMenuInputHandler');
 var registration = require('./registration/registration');
 
-function registerInputHandlers(lang, credit_officers_table) {
+function registerInputHandlers(lang, credit_officers_table, duka_clients_table) {
     addInputHandler(mainMenuInputHandler.handlerName, mainMenuInputHandler.getHandler(lang));
-    registration.registerInputHandlers(lang, credit_officers_table);
+    registration.registerInputHandlers(lang, credit_officers_table, duka_clients_table);
 }
 
-function start(lang) {
+function start(lang, credit_officer_details) {
     var getMessage = translator(translations, lang);
     var message = getMessage('service_title', {}, lang) + getMessage('register_client', {'$label': 1}, lang);
+    state.vars.credit_officer_details = JSON.stringify(credit_officer_details);
     sayText(message);
     promptDigits(mainMenuInputHandler.handlerName);
 }
--- duka-client/dukaClient.test.js
+++ duka-client/dukaClient.test.js
@@ -4,9 +4,10 @@ var registration = require('./registration/registration');
 
 describe.each(['en-ke', 'sw'])('duka client entry using (%s)', (lang) => {
     it('start the application', () => {
-        dukaClient.start(lang);
+        dukaClient.start(lang, {site_id: '123', district_id: '321'});
         const splashScreen = {'sw': 'Chagua huduma\n1) Kusajili Mteja',
             'en-ke': 'Select service\n1) Register Client'};
+        expect(state.vars.credit_officer_details).toEqual('{"site_id":"123","district_id":"321"}');
         expect(sayText).toHaveBeenCalledWith(splashScreen[lang]);
         expect(promptDigits).toHaveBeenCalledWith(mainMenuInputHandler.handlerName);
     });
@@ -15,8 +16,8 @@ describe.each(['en-ke', 'sw'])('duka client entry using (%s)', (lang) => {
         const mainMenuHandler = jest.fn();
         jest.spyOn(registration, 'registerInputHandlers');
         jest.spyOn(mainMenuInputHandler, 'getHandler').mockReturnValue(mainMenuHandler);
-        dukaClient.registerInputHandlers(lang, 'credit_officers_table');
+        dukaClient.registerInputHandlers(lang, 'credit_officers_table', 'duka_clients_table');
         expect(addInputHandler).toHaveBeenCalledWith(mainMenuInputHandler.handlerName, mainMenuHandler);
-        expect(registration.registerInputHandlers).toHaveBeenCalledWith(lang, 'credit_officers_table');
+        expect(registration.registerInputHandlers).toHaveBeenCalledWith(lang, 'credit_officers_table', 'duka_clients_table');
     });
 });
