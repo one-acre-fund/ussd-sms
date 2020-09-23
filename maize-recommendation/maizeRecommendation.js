243d3188a9b329aa3455bb4f8dca76e0dc710a43
BAHATI Robben <bahatiroben@gmail.com>
2020-09-23 13:33:32 +0200

    adding the core maize recommendation

--- maize-recommendation/maizeRecommendation.js
+++ maize-recommendation/maizeRecommendation.js
@@ -1,36 +1,39 @@
  
-var defaultEnvironment;
-if(service.active){
-    defaultEnvironment = 'prod';
-}else{
-    defaultEnvironment = 'dev';
-}
+module.exports = function() {
 
-var env;
-if(service.vars.env === 'prod' || service.vars.env === 'dev'){
-    env = service.vars.env;
-}else{
-    env = defaultEnvironment;
-}
+    var defaultEnvironment;
+    if(service.active){
+        defaultEnvironment = 'prod';
+    }else{
+        defaultEnvironment = 'dev';
+    }
 
-var maize_recommendation_table = project.vars[env + '_maize_recommendation_table'];
+    var env;
+    if(service.vars.env === 'prod' || service.vars.env === 'dev'){
+        env = service.vars.env;
+    }else{
+        env = defaultEnvironment;
+    }
 
-var translations = require('./translations/index');
-var translator = require('../utils/translator/translator');
-var districtResponseHandler = require('./responseHandlers/districtResponseHandler');
-var addResponseHandlers = require('./responseHandlers/addResponseHandlers');
+    var maize_recommendation_table = project.vars[env + '_maize_recommendation_table'];
 
-var lang = contact.vars.lang;
-var getMessage = translator(translations, lang);
-var welcome_message = getMessage('welcome_message', {}, lang);
-var promptDistrict = getMessage('district', {}, lang);
+    var translations = require('./translations/index');
+    var translator = require('../utils/translator/translator');
+    var districtResponseHandler = require('./responseHandlers/districtResponseHandler');
+    var lang = contact.vars.lang;
+    var getMessage = translator(translations, lang);
+    var welcome_message = getMessage('welcome_message', {}, lang);
+    var promptDistrict = getMessage('district', {}, lang);
 
-project.sendMulti({
-    messages: [
-        {content: welcome_message, to_number: contact.phone_number}, 
-        {content: promptDistrict, to_number: contact.phone_number}], 
-    message_type: 'text'
-});
-waitForResponse(districtResponseHandler.handlerName);
-addResponseHandlers(lang, maize_recommendation_table);
- 
+    project.sendMulti({
+        messages: [
+            {content: welcome_message, to_number: contact.phone_number}, 
+            {content: promptDistrict, to_number: contact.phone_number}], 
+        message_type: 'text'
+    });
+    global.waitForResponse(districtResponseHandler.handlerName);
+    return {
+        maize_recommendation_table: maize_recommendation_table,
+        lang: lang
+    };
+};
--- maize-recommendation/maizeRecommendation.test.js
+++ maize-recommendation/maizeRecommendation.test.js
@@ -1,9 +1,9 @@
 var districtResponseHandler = require('./responseHandlers/districtResponseHandler');
 
-describe('Maize recommendation', () => {
+describe.each(['en-ke', 'sw'])('Maize recommendation using (%s)', (lang) => {
 
     beforeAll(() => {
-        global.contact = {vars: {lang: 'en-ke'}};
+        global.contact = {vars: {lang}};
     });
     beforeEach(() => {
         jest.resetModules();
@@ -12,23 +12,30 @@ describe('Maize recommendation', () => {
     it('should send the welcome message and prompt for a district', () => {
         project.vars.dev_maize_recommendation_table = 'maize_recommendation_table';
         contact.phone_number = '0555345';
-        var addResponseHandlers = require('./responseHandlers/addResponseHandlers');
         jest.mock('./responseHandlers/addResponseHandlers');
-        require('./maizeRecommendation');
+        var result = require('./maizeRecommendation')();
+        var messages = [{
+            'en-ke': 'Welcome to the OAF maize seed recommendation. Answer the questions to know the best maize variety for your farm.',
+            'sw': 'Karibu kwenye mashauri na mapendekezo ya mahindi kutoka OAF. Jibu maswali ili ujue aina bora ya mahindi itakayofaa shamba lako.'
+        }, {
+            'en-ke': 'What is your district?',
+            'sw': 'One Acre Fund District yako inaitwaje?'
+        }];
+
         expect(project.sendMulti).toHaveBeenCalledWith({
             'message_type': 'text', 'messages': 
                 [
                     {
-                        'content': 'Welcome to the OAF maize seed recommendation. Answer the questions to know the best maize variety for your farm.',
+                        'content': messages[0][lang],
                         'to_number': '0555345'
                     },
                     {
-                        'content': 'What is your district?',
+                        'content': messages[1][lang],
                         'to_number': '0555345'
                     }
                 ]
         });
+        expect(result).toEqual({'maize_recommendation_table': 'maize_recommendation_table', lang});
         expect(waitForResponse).toHaveBeenCalledWith(districtResponseHandler.handlerName);
-        expect(addResponseHandlers).toHaveBeenCalledWith('en-ke', 'maize_recommendation_table');
     });
 });
