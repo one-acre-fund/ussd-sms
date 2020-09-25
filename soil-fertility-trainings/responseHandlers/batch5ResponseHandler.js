de8bc914c44a73fa5a71e3f8bf0b403fa683941a
BAHATI Robben <bahatiroben@gmail.com>
2020-09-25 09:09:58 +0200

    adding global to the telerivets variables in batch5, batch6 and batch6  response handlers

--- soil-fertility-trainings/responseHandlers/batch5ResponseHandler.js
+++ soil-fertility-trainings/responseHandlers/batch5ResponseHandler.js
@@ -1,27 +1,26 @@
 
 var translations = require('../translations/index');
 var translator = require('../../utils/translator/translator');
-var batch6ResponseHandler = require('./batch6ResponseHandler');
 
 var HandlerName = 'sms_5_response';
 module.exports = {
     handlerName: HandlerName,
     getHandler: function(lang) {
         return function() {
-
+            var batch6ResponseHandler = require('./batch6ResponseHandler');
             var getMessage = translator(translations, lang);
             var response = content.trim().toUpperCase();
 
             if(response == 'A') {
-                sendReply(getMessage('sms-5.6', {}, lang));
+                global.sendReply(getMessage('sms-5.6', {}, lang));
             } else if(response == 'B') {
-                sendReply(getMessage('sms-5.7', {}, lang));
+                global.sendReply(getMessage('sms-5.7', {}, lang));
             } else {
-                sendReply(getMessage('invalid_rotate_answer', {}, lang));
-                waitForResponse(HandlerName);
+                global.sendReply(getMessage('invalid_rotate_answer', {}, lang));
+                global.waitForResponse(HandlerName);
                 return;
             }
-            waitForResponse(batch6ResponseHandler.handlerName);
+            global.waitForResponse(batch6ResponseHandler.handlerName);
         };
     }
 };
