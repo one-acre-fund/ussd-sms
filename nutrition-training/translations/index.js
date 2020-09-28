d0285c8b645f2c00558f7c6c4e3d484aa7285078
BAHATI Robben <bahatiroben@gmail.com>
2020-09-28 17:24:40 +0200

    updating the translations file with english translations

--- nutrition-training/translations/index.js
+++ nutrition-training/translations/index.js
@@ -4,15 +4,15 @@ module.exports = {
         'sw': 'Ujumbe wa mafunzo umetumwa kwa simu yako. SMS zote ni bure! Kumbuka kufuta SMS usizohitaji ili uwe na nafasi ya kupata ujumbe zaidi. '
     },
     'sms_1.1': {
-        'en-ke': '(1/6) Leo tutasoma juu ya lishe bora.Lishe bora huzuia upungufu wa damu mwilini/ damu dhaifu na inaboresha nguvu na afya ya familia nzima!',
+        'en-ke': '(1/6)Today we will learn about blanaced diets. Eating like this prevents anemia and improves the energy and health of the whole family!',
         'sw': '(1/6) Leo tutasoma juu ya lishe bora.Lishe bora huzuia upungufu wa damu mwilini/ damu dhaifu na inaboresha nguvu na afya ya familia nzima!'
     },
     'sms_1.2': {
-        'en-ke': '(2/6)Ni ipi kati ya vyakula vifuatavyo vina madini zaidi ya kuongeza damu mwilini?\n' +
-        'a. Maharagwe\n' +
-        'b. Ugali\n' +
-        'c. Mchele\n' +
-        'Jibu na  a, b, au c\n',
+        'en-ke': '(2/6) Which of the following foods has the most iron?' + 
+        'a. Beans\n' + 
+        'b. Ugali\n' + 
+        'c. Rice\n' + 
+        'Reply with either a,b, or c',
         'sw': '(2/6)Ni ipi kati ya vyakula vifuatavyo vina madini zaidi ya kuongeza damu mwilini?\n' +
         'a. Maharagwe\n' +
         'b. Ugali\n' +
@@ -20,23 +20,23 @@ module.exports = {
         'Jibu na  a, b, au c'
     },
     'sms_1.3': {
-        'en-ke': 'Sahihi! maharagwe, mayai, omena na mboga za majani (managu) vina madini ya kuongeza damu. Madini haya hufanya mtoto wako kuwa mwerevu!',
+        'en-ke': 'Correct! Beans, eggs, omena and green leafy vegetables (Managu, Saga) are very good sources of iron. Iron makes your child smart!',
         'sw': 'Sahihi! maharagwe, mayai, omena na mboga za majani (managu) vina madini ya kuongeza damu. Madini haya hufanya mtoto wako kuwa mwerevu!'
     },
     'sms_1.4': {
-        'en-ke': 'Maharagwe yana madini ya kuongeza damu. Ugali / Mchele hauna madini ya kuongeza damu vya kutosha kuzuia upungufu wa damu mwilini',
+        'en-ke': 'Actually, beans have the most iron. Ugali/Rice make you full but do not contain enough iron to prevent anemia and imrove health',
         'sw': 'Maharagwe yana madini ya kuongeza damu. Ugali / Mchele hauna madini ya kuongeza damu vya kutosha kuzuia upungufu wa damu mwilini'
     },
     'sms_1.5': {
-        'en-ke': 'Mayai, omena, na mboga vina madini ya kuongeza damu mwilini. Kula pamoja na ugali na mchele kufanya mtoto wako awe mwerevu!',
+        'en-ke': 'Eggs, omena, and green leafy vegetables (managu, saga) are good sources of iron. Eat them with ugali & rice to make your child smart!',
         'sw': 'Mayai, omena, na mboga vina madini ya kuongeza damu mwilini. Kula pamoja na ugali na mchele kufanya mtoto wako awe mwerevu!'
     },
     'sms_1.6': {
-        'en-ke': '(3/6)Vyakula vyenye madini ya kuongeza damu mwilini huzuia ugonjwa upi?\n' +
-        'a. Damu dhaifu\n' +
-        'b. Upofu wa usiku\n' +
-        'c. Mifupa dhaifu\n' +
-        'Jibu na a, b au c',
+        'en-ke': 'Which disease can we prevent by eating iron rich foods?\n' + 
+        'a. Anemia or weak blood\n' + 
+        'b. Night blindness\n' + 
+        'c. Weak bones\n' +
+        'Reply with a,b or c',
         'sw': '(3/6)Vyakula vyenye madini ya kuongeza damu mwilini huzuia ugonjwa upi?\n' +
         'a. Damu dhaifu\n' +
         'b. Upofu wa usiku\n' +
@@ -44,23 +44,27 @@ module.exports = {
         'Jibu na a, b au c'
     },
     'sms_1.7': {
-        'en-ke': 'Sahihi! damu dhaifu ipo kwa jamii na familia zetu na inafanya watoto wawe na ukuaji duni, kufanya vibaya shuleni na  wenye afya duni.',
+        'en-ke': 'Correct! Anemia (weak blood) is common in our families. It makes children develop poorly so they do badly in school and are unhealthy',
         'sw': 'Sahihi! damu dhaifu ipo kwa jamii na familia zetu na inafanya watoto wawe na ukuaji duni, kufanya vibaya shuleni na  wenye afya duni.'
     },
     'sms_1.8': {
-        'en-ke': 'Jibu sahihi ni upungufu wa damu mwilini. Kula chakula cha kutosha chenye madini ya kuongeza damu husababisha upungufu wa damu mwilini',
+        'en-ke': 'The correct answer is anemia or weak blood. Not eating enough iron-rich foods causes anemia.',
         'sw': 'Jibu sahihi ni upungufu wa damu mwilini. Kula chakula cha kutosha chenye madini ya kuongeza damu husababisha upungufu wa damu mwilini'
     },
     'sms_1.9': {
-        'en-ke': '(4/6)Zuia damu dhaifu kwa kula vyakula vyenye madini ya kuongeza damu. Vyakula hivi hufanya mtoto kua mchangamfu, mwenye nguvu na mwerevu',
+        'en-ke': 'Anemia can be prevented by eating iron rich foods. Iron-rich foods makes your child active, strong and bright',
         'sw': '(4/6)Zuia damu dhaifu kwa kula vyakula vyenye madini ya kuongeza damu. Vyakula hivi hufanya mtoto kua mchangamfu, mwenye nguvu na mwerevu'
     },
     'sms_2.1': {
-        'en-ke': '(5/6)Unaweza kupata mayai, maharagwe na mboga kutoka shamba lako na kuokoa pesa! Panda maharagwe na ufuge kuku wa mayai. ',
+        'en-ke': 'You can get eggs, beans and vegetables from your farm and save money! Plant beans in small quantities and rear chicken for eggs.',
         'sw': '(5/6)Unaweza kupata mayai, maharagwe na mboga kutoka shamba lako na kuokoa pesa! Panda maharagwe na ufuge kuku wa mayai. '
     },
     'sms_2.2': {
-        'en-ke': '(6/6)Hongera! Umemaliza mafunzo ya lishe bora! ZIngatia masomo haya kwa familia yako ili wawe na afya nzuri!',
+        'en-ke': 'Contragulations! You have completed the training on dietary diversity! Practice what you have learnt in this lesson for a healthy family',
         'sw': '(6/6)Hongera! Umemaliza mafunzo ya lishe bora! ZIngatia masomo haya kwa familia yako ili wawe na afya nzuri!'
+    },
+    'invalid_choice': {
+        'en-ke': 'Reply with a or b to answer',
+        'sw': 'Chagua a au b kujibu!'
     }
 };
