d337c075a0844e812ef7bc19c6d2ab2666c33fe7
BAHATI Robben <bahatiroben@gmail.com>
2020-10-02 12:07:30 +0200

    validating the first name

--- duka-client/registration/inputHandlers/firstNameInputHandler.js
+++ duka-client/registration/inputHandlers/firstNameInputHandler.js
@@ -10,8 +10,13 @@ module.exports = {
         return function(input) {
             input = input.replace(/[^a-zA-Z0-9]/gi, '');
             notifyElk();
-            var secondNameInputHandler = require('./secondNameInputHandler');
             var getMessage = translator(translations, lang);
+            if(!input) {
+                global.sayText(getMessage('enter_first_name', {}, lang));
+                global.promptDigits(handlerName);
+                return;
+            }
+            var secondNameInputHandler = require('./secondNameInputHandler');
             state.vars.duka_client_first_name = input;
             var secondNamePrompt = getMessage('enter_second_name', {}, lang);
             global.sayText(secondNamePrompt);
--- duka-client/registration/inputHandlers/firstNameInputHandler.test.js
+++ duka-client/registration/inputHandlers/firstNameInputHandler.test.js
@@ -4,7 +4,7 @@ const notifyElk = require('../../../notifications/elk-notification/elkNotificati
 
 jest.mock('../../../notifications/elk-notification/elkNotification');
 describe.each(['en-ke', 'sw'])('first name input handler', (lang) => {
-    it('should prompt the user for first name once the phone number is valid', () => {
+    it('should prompt the user for the second name once the first name is valid', () => {
         const handler = firstNameInputHandler.getHandler(lang);
         const message = {
             'sw': 'Tafadhali jibu na jina la pili la mteja.',
@@ -16,4 +16,18 @@ describe.each(['en-ke', 'sw'])('first name input handler', (lang) => {
         expect(state.vars.duka_client_first_name).toEqual('Jamie1');
         expect(promptDigits).toHaveBeenCalledWith(secondNameInputHandler.handlerName);
     });
+
+    it('should reprompt for the first name once the input is invalid', () => {
+        const handler = firstNameInputHandler.getHandler(lang);
+        const message = {
+            'en-ke': 'Please reply with the first name of the client.',
+            'sw': 'Tafadhali jibu na jina la kwanza la mteja.'
+        };
+        handler('`*& ^_ ');
+        expect(notifyElk).toHaveBeenCalled();
+        expect(sayText).toHaveBeenCalledWith(message[lang]);
+        expect(promptDigits).toHaveBeenCalledWith(firstNameInputHandler.handlerName);
+    });
+
+    
 });
