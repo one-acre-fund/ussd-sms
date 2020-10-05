03fe442f4a3871b1db65467d28037852955f50a7
BAHATI Robben <bahatiroben@gmail.com>
2020-10-05 16:17:03 +0200

    updating batch 4 response handler

--- soil-fertility-trainings/responseHandlers/batch4ResponseHandler.js
+++ soil-fertility-trainings/responseHandlers/batch4ResponseHandler.js
@@ -11,9 +11,11 @@ module.exports = {
             var getMessage = translator(translations, lang);
             var messages = ['sms-5.1', 'sms-5.2', 'sms-5.3', 'sms-5.4', 'sms-5.5'];
 
-            messages.forEach(function(message) {
-                global.sendReply(getMessage(message, {}, lang));
-            });
+            global.sendReply(getMessage(messages[0], {}, lang));
+            global.sendReply(getMessage(messages[1], {}, lang));
+            global.sendReply(getMessage(messages[2], {}, lang));
+            global.sendReply(getMessage(messages[3], {}, lang));
+            global.sendReply(getMessage(messages[4], {}, lang));
 
             global.waitForResponse(batch5ResponseHandler.handlerName);
         };
