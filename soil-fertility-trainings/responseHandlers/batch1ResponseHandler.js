e079200624359c52e59d7b7e64c83c2c453b9e67
BAHATI Robben <bahatiroben@gmail.com>
2020-10-09 14:20:50 +0200

    updating the batch 1 messages to use sendMulti

--- soil-fertility-trainings/responseHandlers/batch1ResponseHandler.js
+++ soil-fertility-trainings/responseHandlers/batch1ResponseHandler.js
@@ -10,11 +10,29 @@ module.exports = {
             var batch2ResponseHandler = require('./batch2ResponseHandler');
             var getMessage = translator(translations, lang);
             var messages = ['sms-2.1', 'sms-2.2', 'sms-2.3', 'sms-2.4'];
+            project.sendMulti({
+                messages: [{
+                    'content': getMessage(messages[0], {}, lang),
+                    'to_number': contact.phone_number,
+                    'priority': 2,
+                },
+                {
+                    'content': getMessage(messages[1], {}, lang),
+                    'to_number': contact.phone_number,
+                    'priority': 2,
+                },
+                {
+                    'content': getMessage(messages[2], {}, lang),
+                    'to_number': contact.phone_number,
+                    'priority': 2,
+                },
+                {
+                    'content': getMessage(messages[3], {}, lang),
+                    'to_number': contact.phone_number,
+                    'priority': 2,
+                }]
 
-            global.sendReply(getMessage(messages[0], {}, lang));
-            global.sendReply(getMessage(messages[1], {}, lang));
-            global.sendReply(getMessage(messages[2], {}, lang));
-            global.sendReply(getMessage(messages[3], {}, lang));
+            });
 
             global.waitForResponse(batch2ResponseHandler.handlerName);
         };
--- soil-fertility-trainings/responseHandlers/batch1ResponseHandler.test.js
+++ soil-fertility-trainings/responseHandlers/batch1ResponseHandler.test.js
@@ -4,12 +4,18 @@ const batch2ResponseHandler = require('./batch2ResponseHandler');
 describe('batch1 messages response handler', () => {
     it('should send all 2 messages once the user responds to the batch 1 messages', () => {
         var lang = 'en-ke';
+        contact.phone_number = '0780475911';
         var batch1Handler = batch1ResponseHandler.getHandler(lang);
         batch1Handler();
-        expect(sendReply).toHaveBeenNthCalledWith(1, 'First, let\'s learn about the different types of soils. Every type of soil is made up of three main parts: sand, silt and clay.');
-        expect(sendReply).toHaveBeenNthCalledWith(2, 'Soils with equal amount of sand, silt and clay are called loam soils. These are the best type of soils.');
-        expect(sendReply).toHaveBeenNthCalledWith(3, 'Sandy soils have more sand, drains water easily and are not very fertile. Clay soils have more clay and drains poorly when wet.');
-        expect(sendReply).toHaveBeenNthCalledWith(4, 'All types of soils need to be improved for you to have great harvests. You can improve your soil by adding organic matter!');
+        expect(project.sendMulti).toHaveBeenCalledWith({'messages': [
+            {'content': 'First, let\'s learn about the different types of soils. Every type of soil is made up of three main parts: sand, silt and clay.',
+                'priority': 2, 'to_number': '0780475911'},
+            {'content': 'Soils with equal amount of sand, silt and clay are called loam soils. These are the best type of soils.',
+                'priority': 2, 'to_number': '0780475911'}, 
+            {'content': 'Sandy soils have more sand, drains water easily and are not very fertile. Clay soils have more clay and drains poorly when wet.',
+                'priority': 2, 'to_number': '0780475911'}, 
+            {'content': 'All types of soils need to be improved for you to have great harvests. You can improve your soil by adding organic matter!',
+                'priority': 2, 'to_number': '0780475911'}]});
         expect(waitForResponse).toHaveBeenCalledWith(batch2ResponseHandler.handlerName);
     });
 });
