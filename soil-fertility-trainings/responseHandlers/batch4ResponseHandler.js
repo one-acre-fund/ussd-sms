825a74291ce71656aaf9de555a4f2e366faaf604
BAHATI Robben <bahatiroben@gmail.com>
2020-10-09 14:57:40 +0200

    updating the batch 4 with sendMulti

--- soil-fertility-trainings/responseHandlers/batch4ResponseHandler.js
+++ soil-fertility-trainings/responseHandlers/batch4ResponseHandler.js
@@ -11,11 +11,34 @@ module.exports = {
             var getMessage = translator(translations, lang);
             var messages = ['sms-5.1', 'sms-5.2', 'sms-5.3', 'sms-5.4', 'sms-5.5'];
 
-            global.sendReply(getMessage(messages[0], {}, lang));
-            global.sendReply(getMessage(messages[1], {}, lang));
-            global.sendReply(getMessage(messages[2], {}, lang));
-            global.sendReply(getMessage(messages[3], {}, lang));
-            global.sendReply(getMessage(messages[4], {}, lang));
+            project.sendMulti({
+                messages: [
+                    {
+                        'content': getMessage(messages[0], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[1], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[2], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[3], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },                     {
+                        'content': getMessage(messages[4], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 1,
+                    }
+                ]
+            });
 
             global.waitForResponse(batch5ResponseHandler.handlerName);
         };
--- soil-fertility-trainings/responseHandlers/batch4ResponseHandler.test.js
+++ soil-fertility-trainings/responseHandlers/batch4ResponseHandler.test.js
@@ -4,13 +4,23 @@ const batch5ResponseHandler = require('./batch5ResponseHandler');
 describe('batch 4 messages response handler', () => {
     it('should send all 5 batch 5 messages once the user responds to the batch 4 messages', () => {
         var lang = 'en-ke';
+        contact.phone_number = '0780475911';
         var batch4Handler = batch4ResponseHandler.getHandler(lang);
         batch4Handler();
-        expect(sendReply).toHaveBeenNthCalledWith(1, 'You can also improve the quality of soil by planting legumes like beans, groundnuts, that put nutrients to your soil when they are growing.');
-        expect(sendReply).toHaveBeenNthCalledWith(2, 'You can intercrop these crops with your maize in the long rains season, or practice crop rotation.');
-        expect(sendReply).toHaveBeenNthCalledWith(3, 'To practice crop rotation, avoid planting the same crop on the same piece of land every year. Rotate legumes with cereals like maize.');
-        expect(sendReply).toHaveBeenNthCalledWith(4, 'Other crops that significantly improve your soil quality include Grevillea Trees, Sweet Potatoes and Sunflowers.');
-        expect(sendReply).toHaveBeenNthCalledWith(5, 'Why should we rotate our crops?\nA. To prevent pests and disease building up in our soil and have bigger harvests!\nB. To reduce soil health.');
+        expect(project.sendMulti).toHaveBeenCalledWith({'messages': 
+        [{'content': 'You can also improve the quality of soil by planting legumes like beans, groundnuts, that put nutrients to your soil when they are growing.',
+            'priority': 2, 
+            'to_number': '0780475911'},
+        {'content': 'You can intercrop these crops with your maize in the long rains season, or practice crop rotation.', 
+            'priority': 2, 
+            'to_number': '0780475911'},
+        {'content': 'To practice crop rotation, avoid planting the same crop on the same piece of land every year. Rotate legumes with cereals like maize.',
+            'priority': 2,
+            'to_number': '0780475911'}, 
+        {'content': 'Other crops that significantly improve your soil quality include Grevillea Trees, Sweet Potatoes and Sunflowers.', 
+            'priority': 2, 
+            'to_number': '0780475911'}, 
+        {'content': 'Why should we rotate our crops?\nA. To prevent pests and disease building up in our soil and have bigger harvests!\nB. To reduce soil health.', 'priority': 1, 'to_number': '0780475911'}]});
         expect(waitForResponse).toHaveBeenCalledWith(batch5ResponseHandler.handlerName);
     });
 });
