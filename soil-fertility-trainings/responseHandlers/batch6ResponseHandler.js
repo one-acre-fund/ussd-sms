ce35e59f5e87c0ee7e5994d6c2315545f5372bc5
BAHATI Robben <bahatiroben@gmail.com>
2020-10-09 15:09:37 +0200

    updating the batch 6 to use send Multi

--- soil-fertility-trainings/responseHandlers/batch6ResponseHandler.js
+++ soil-fertility-trainings/responseHandlers/batch6ResponseHandler.js
@@ -11,10 +11,30 @@ module.exports = {
             var getMessage = translator(translations, lang);
             var messages = ['sms-6.1', 'sms-6.2', 'sms-6.3', 'sms-6.4'];
 
-            global.sendReply(getMessage(messages[0], {}, lang));
-            global.sendReply(getMessage(messages[1], {}, lang));
-            global.sendReply(getMessage(messages[2], {}, lang));
-            global.sendReply(getMessage(messages[3], {}, lang));
+            project.sendMulti({
+                messages: [
+                    {
+                        'content': getMessage(messages[0], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[1], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[2], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    },
+                    {
+                        'content': getMessage(messages[3], {}, lang),
+                        'to_number': contact.phone_number,
+                        'priority': 2,
+                    }
+                ]
+            });
 
             global.waitForResponse(batch7ResponseHandler.handlerName);
         };
--- soil-fertility-trainings/responseHandlers/batch6ResponseHandler.test.js
+++ soil-fertility-trainings/responseHandlers/batch6ResponseHandler.test.js
@@ -4,12 +4,22 @@ const batch7ResponseHandler = require('./batch7ResponseHandler');
 describe('batch 6 messages response handler', () => {
     it('should send all 4 batch 6 messages once the user responds to the batch 5 messages', () => {
         var lang = 'en-ke';
+        contact.phone_number = '0780475911';
         var batch6Handler = batch6ResponseHandler.getHandler(lang);
         batch6Handler();
-        expect(sendReply).toHaveBeenNthCalledWith(1, 'For healthier soil, manage soil acidity. Soil acidity is a condition that makes it difficult for plants to access the proper nutrients.');
-        expect(sendReply).toHaveBeenNthCalledWith(2, 'Soil acidity occurs when you have high rainfall, erosion and lack of crop rotation. It is worse when you do not have enough organic matter.');
-        expect(sendReply).toHaveBeenNthCalledWith(3, 'Signs of high soil acidity include low yields and yellow plants in the farm even when you apply fertilizer. Also prone to pests and diseases.');
-        expect(sendReply).toHaveBeenNthCalledWith(4, 'To correct soil acidity, Apply compost, Control erosion and Apply lime (soil improver). Talk to your FO to purchase lime for your farm!');
+        expect(project.sendMulti).toHaveBeenCalledWith({'messages': 
+        [{'content': 'For healthier soil, manage soil acidity. Soil acidity is a condition that makes it difficult for plants to access the proper nutrients.',
+            'priority': 2, 
+            'to_number': '0780475911'}, 
+        {'content': 'Soil acidity occurs when you have high rainfall, erosion and lack of crop rotation. It is worse when you do not have enough organic matter.',
+            'priority': 2,
+            'to_number': '0780475911'},
+        {'content': 'Signs of high soil acidity include low yields and yellow plants in the farm even when you apply fertilizer. Also prone to pests and diseases.',
+            'priority': 2,
+            'to_number': '0780475911'}, 
+        {'content': 'To correct soil acidity, Apply compost, Control erosion and Apply lime (soil improver). Talk to your FO to purchase lime for your farm!', 
+            'priority': 2, 
+            'to_number': '0780475911'}]});
         expect(waitForResponse).toHaveBeenCalledWith(batch7ResponseHandler.handlerName);
     });
 });
