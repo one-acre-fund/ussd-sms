f893588c005b0769a535601c47023c6cf8420582
BAHATI Robben <bahatiroben@gmail.com>
2020-10-09 16:14:53 +0200

    updating the healthy path on repayments

--- healthy-path/repayments/HealthyPathOnRepaymentReceipts.js
+++ healthy-path/repayments/HealthyPathOnRepaymentReceipts.js
@@ -1,17 +1,17 @@
 var calculateHealthyPath = require('../utils/healthyPathCalculator');
 var translations = require('../translations/index');
 var translator = require('../../utils/translator/translator');
-var fetchHealthyPath = require('../utils/fetchHealthyPathData');
+var getHealthyPathPercentage = require('../utils/getHealthyPathPercentage');
 
-/**
+/** 
  * generates a healthy path message to append on repayment receipt message
  * @param {Number} credit the total credit
  * @param {Number} repaid the total repaid
  * @param {String} lang the language to be used 
  */
-module.exports = function(credit, repaid, lang) {
+module.exports = function(SeasonId, CountryId, DistrictId, credit, repaid, lang) {
     var getMessage = translator(translations, lang);
-    var hp_percentage = fetchHealthyPath();
+    var hp_percentage = getHealthyPathPercentage(SeasonId, CountryId, DistrictId);
     var healthyPathDistance = calculateHealthyPath(hp_percentage, credit, repaid);
     var message = '';
     if(healthyPathDistance > 0) {
--- healthy-path/repayments/healthyPathOnRepayments.test.js
+++ healthy-path/repayments/healthyPathOnRepayments.test.js
@@ -1,14 +1,18 @@
 const HealthyPathMessage = require('./HealthyPathOnRepaymentReceipts');
-jest.mock('../utils/fetchHealthyPathData');
-require('../utils/fetchHealthyPathData').mockImplementation(() => 0.3);
+var getHealthyPathPercentage = require('../utils/getHealthyPathPercentage');
+jest.mock('../utils/getHealthyPathPercentage');
 
 describe('Healthy path on Repayments receipt', () => {
+    beforeAll(() => {
+        getHealthyPathPercentage.mockReturnValue(0.3);
+    });
+
     it('should return a message when the user is below the healthy path', () => {
-        var message = HealthyPathMessage(150, 24, 'en');
+        var message = HealthyPathMessage(1, 2, 3, 150, 24, 'en');
         expect(message).toBe('Pay 21 to stay on the healthy path\n');
     });
     it('should return an empty message when the user is above the healthy path', () => {
-        var message = HealthyPathMessage(150, 50, 'en');
+        var message = HealthyPathMessage(1, 2, 3, 150, 50, 'en');
         expect(message).toBe('');
     });
 });
