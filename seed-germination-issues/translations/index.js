7a51de4e2fe699c4800fb6b5338b22bfe90ab084
BAHATI Robben <bahatiroben@gmail.com>
2020-10-26 11:29:41 +0200

    adding the seed germination issues reporting

---
+++ seed-germination-issues/translations/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'seeds_title': {
+        'en-ke': 'Which seed brand did you purchase? Please select number\n$Menu',
+        'sw': 'Which seed brand did you purchase? Please select number\n$Menu'
+    }
+};
