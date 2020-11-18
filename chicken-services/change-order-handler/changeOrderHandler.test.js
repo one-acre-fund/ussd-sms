01f36384d465209e38197a42c28c507dc18bf7f3
BAHATI Robben <bahatiroben@gmail.com>
2020-11-18 08:30:50 +0200

    replacing the project variable language with service variable lang in change order confirmation

--- chicken-services/change-order-handler/changeOrderHandler.test.js
+++ chicken-services/change-order-handler/changeOrderHandler.test.js
@@ -1,3 +1,4 @@
+service.vars.lang = 'en';
 const {handlerName,getHandler} = require ('./changeOrderHandler');
 var notifyELK = require('../../notifications/elk-notification/elkNotification'); 
 var backToMain = require('../../rw-legacy/lib/backToMainMenu');
