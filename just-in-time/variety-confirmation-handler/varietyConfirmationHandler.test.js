ee10e86699db58369a5e53824d9f3e1f316fdad4
SabinSheja <sabin.sheja@oneacrefund.org>
2020-11-20 09:30:59 +0200

    adding unit tests

--- just-in-time/variety-confirmation-handler/varietyConfirmationHandler.test.js
+++ just-in-time/variety-confirmation-handler/varietyConfirmationHandler.test.js
@@ -1,15 +1,18 @@
 var {getHandler} = require('./varietyConfirmationHandler');
 var notifyELK = require('../../notifications/elk-notification/elkNotification');
-
+var bundleChoiceHandler = require('../bundle-choice-handler/bundleChoiceHandler');
+var {client} = require('../../client-enrollment/test-client-data'); 
 httpClient.request.mockReturnValue({status: 200});
 jest.mock('../../notifications/elk-notification/elkNotification');
 describe('order confirmation handler test', ()=>{
 
     var varietyConfirmationHandler;
-    var onBundleSelected;
+    var onBundleSelected,displayBundles;
     beforeAll(()=>{
+        state.vars.topUpClient = JSON.stringify(client);
         onBundleSelected = jest.fn();
-        varietyConfirmationHandler = getHandler(onBundleSelected);
+        displayBundles = jest.fn();
+        varietyConfirmationHandler = getHandler(onBundleSelected,displayBundles);
         state.vars.chosenVariety = JSON.stringify([{bundleId: '-2711',bundleInputId: '-11823'}]);
     });
 
@@ -20,5 +23,10 @@ describe('order confirmation handler test', ()=>{
     it('should call on bundle selected function if the user chooses 1',()=>{
         varietyConfirmationHandler(1);
         expect(onBundleSelected).toHaveBeenCalledWith(JSON.parse(state.vars.chosenVariety).bundleId,true, JSON.parse(state.vars.chosenVariety).bundleInputId);
+    });
+    it('should call on display bundles and prompt for bundle choice if the user did not chooses 1(confim)',()=>{
+        varietyConfirmationHandler(2);
+        expect(displayBundles).toHaveBeenCalledWith(client.DistrictId);
+        expect(promptDigits).toHaveBeenCalledWith(bundleChoiceHandler.handlerName);
     });
 });
