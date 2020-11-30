739d8ab2b25d94e7c42ae09370ad9ee47f1020a9
Victor Bassey <viktorbasi545@gmail.com>
2020-11-30 10:45:10 +0200

    fix linting errors

--- shared/rosterApi/verifyGroup.js
+++ shared/rosterApi/verifyGroup.js
@@ -1,4 +1,4 @@
-var Log = require("../../logger/elk/elk-logger");
+var Log = require('../../logger/elk/elk-logger');
 
 /**
  *
@@ -8,35 +8,35 @@ var Log = require("../../logger/elk/elk-logger");
  * @returns {object} an object containing the group name and active status
  */
 module.exports = function verifyGroup(districtId, siteId, groupId) {
-	var logger = new Log();
-	var fullUrl =
-		service.vars.server_name +
-		"/Api/GroupSummary/VerifyGroup?districtId=" +
-		districtId +
-		"&siteId=" +
-		siteId +
-		"&groupId=" +
-		groupId;
+    var logger = new Log();
+    var fullUrl =
+        service.vars.server_name +
+        '/Api/GroupSummary/VerifyGroup?districtId=' +
+        districtId +
+        '&siteId=' +
+        siteId +
+        '&groupId=' +
+        groupId;
 
-	var opts = { headers: {} };
-	opts.headers["Authorization"] = "ApiKey " + service.vars.roster_read_key;
-	opts.headers["Content-Type"] = "application/json";
-	opts.method = "GET";
-	try {
-		var response = httpClient.request(fullUrl, opts);
-		if (response.status === 200) {
-			var data = JSON.parse(response.content);
-			return data;
-		} else {
-			console.log("Error while verifying group");
-			console.log(JSON.stringify(response));
-			logger.error("Error while verifying group", {
-				data: JSON.stringify(response),
-			});
-		}
-	} catch (error) {
-		logger.error("API Error while verifying group", {
-			data: JSON.stringify(error),
-		});
-	}
+    var opts = { headers: {} };
+    opts.headers['Authorization'] = 'ApiKey ' + service.vars.roster_read_key;
+    opts.headers['Content-Type'] = 'application/json';
+    opts.method = 'GET';
+    try {
+        var response = httpClient.request(fullUrl, opts);
+        if (response.status === 200) {
+            var data = JSON.parse(response.content);
+            return data;
+        } else {
+            console.log('Error while verifying group');
+            console.log(JSON.stringify(response));
+            logger.error('Error while verifying group', {
+                data: JSON.stringify(response),
+            });
+        }
+    } catch (error) {
+        logger.error('API Error while verifying group', {
+            data: JSON.stringify(error),
+        });
+    }
 };
