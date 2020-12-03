9eee68b5ab4b6d75924c8174e28253f67e82a49a
Angello Obel <angello.obel@oneacrefund.org>
2020-12-03 14:38:35 +0300

    cleaned up mocks some more

--- repayments/repayments.test.js
+++ repayments/repayments.test.js
@@ -84,9 +84,8 @@ const fullyPaidClient = {
 project.getOrCreateLabel.mockImplementation((label)=>({id: label}));
 
 describe('mobile money repayments using', () => {
-    function setupLabels() {
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-    }
+    var mockedRow = {save: jest.fn(),hasNext: jest.fn(() => true), next: jest.fn(),vars: {}, limit: jest.fn()}; 
+    var mockedTable = { queryRows: () => mockedRow};
     beforeAll(() => {
         global.state = { vars: {} };
         contact.phone_number = '0755432334';
@@ -94,15 +93,13 @@ describe('mobile money repayments using', () => {
     });
     beforeEach(() => {
         jest.resetModules();
+        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
         getHealthyPathPercentage = require('../healthy-path/utils/getHealthyPathPercentage');
         getPhoneNumber = require('../shared/rosterApi/getPhoneNumber');
         project.sendMessage.mockClear();
     });
-    var mockedRow = {save: jest.fn(),hasNext: jest.fn(() => true), next: jest.fn(),vars: {}, limit: jest.fn()}; 
-    var mockedTable = { queryRows: () => mockedRow};
 
     it('should send an shs notification to an active phone once all the debt is paid', () => {
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
         getPhoneNumber.mockReturnValueOnce([ {PhoneNumber: '05423827342', IsInactive: true}, {PhoneNumber: '075342312', IsInactive: false}]);
         contact.vars = {
             accountnumber: '3033-cf74-f94a',
@@ -118,10 +115,7 @@ describe('mobile money repayments using', () => {
     });
 
     it('should send a message once there is an over payment', () => {
-        getHealthyPathPercentage.mockReturnValueOnce(0.3);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-        setupLabels();
-
+        getHealthyPathPercentage.mockReturnValueOnce(0.3);        
         contact.vars = overpaidContact;
         require('./repayments');
         expect(project.sendMessage).toHaveBeenCalledWith({'content': 'Je-3033-cf74-f94a You completed your current loan. Last Payment: KSh 3000. Receipt number 5beb94c0-3033-cf74-f94a. Total Paid toward your next loan: Ksh 2000.',
@@ -135,10 +129,7 @@ describe('mobile money repayments using', () => {
     });
 
     it('should send a message  with health path message once there loan is not fullly paid', () => {
-        getHealthyPathPercentage.mockReturnValueOnce(0.9);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-        setupLabels();
-
+        getHealthyPathPercentage.mockReturnValueOnce(0.9);        
         contact.vars = contactNotFullyPaid;
         require('./repayments');
         expect(project.sendMessage).toHaveBeenCalledWith({'content': 'Hello Je-3033-cf74-f94a Last payment: KSh 3000. Receipt Number 5beb94c0-3033-cf74-f94a. Total paid KSh 14000. Balance KSh 6500. Pay 1500 to stay on the healthy path.',
@@ -151,9 +142,7 @@ describe('mobile money repayments using', () => {
     });
     it('should send a message in swahili once there is no data in the EnglishDistricts', () => {
         getHealthyPathPercentage.mockReturnValueOnce(0.3);
-        mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-        setupLabels();
+        mockedRow.hasNext = jest.fn(() => false);        
         contact.vars = overpaidContact;
         require('./repayments');
         expect(project.sendMessage).toHaveBeenCalledWith({'content': 'Je-3033-cf74-f94a Ulikamilisha malipo ya mkopo wa sasa. Malipo ya mwisho: KSh 3000. Nambari ya risiti 5beb94c0-3033-cf74-f94a. Malipo kwa ujumla kulipia mkopo unaofuata KSh 2000.',
@@ -169,9 +158,7 @@ describe('mobile money repayments using', () => {
         getHealthyPathPercentage.mockReturnValueOnce(0.3);
         const accountPhoneNumber = '05423827342';
         getPhoneNumber.mockReturnValueOnce([ {PhoneNumber: accountPhoneNumber, IsInactive: false}]);
-        mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-        setupLabels();
+        mockedRow.hasNext = jest.fn(() => false);        
         contact.vars = overpaidContact;
         require('./repayments');
         expect(project.sendMessage).toHaveBeenCalledTimes(2);
@@ -188,21 +175,18 @@ describe('mobile money repayments using', () => {
         getHealthyPathPercentage.mockReturnValueOnce(0.3);
         const accountPhoneNumber = contact.phone_number;
         getPhoneNumber.mockReturnValueOnce([ {PhoneNumber: accountPhoneNumber, IsInactive: false}]);
-        mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
-        setupLabels();
+        mockedRow.hasNext = jest.fn(() => false);        
         contact.vars = overpaidContact;
         require('./repayments');
         expect(project.sendMessage).toHaveBeenCalledTimes(1);
     });
-
+    
     it('should log an error once the phone number is not found', () => {
         mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
         var logger = require('../slack-logger/index');
         jest.mock('../slack-logger/index');
         jest.spyOn(logger, 'log');
-
+        
         contact.vars = contactWithoutPhoneNumber;
         require('./repayments');
         expect(logger.log).toHaveBeenCalledWith('error in shs notification: could not get a to_phone number from roster');
@@ -210,23 +194,21 @@ describe('mobile money repayments using', () => {
 
     it('should validate the project variables', () => {
         mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
         var validateProjectVatiables = require('./validateProjectVariables');
         jest.mock('./validateProjectVariables');
-
+        
         require('./repayments');
         
         expect(validateProjectVatiables).toHaveBeenCalledWith('dev');
     });
-
+    
     it('should validate the project variables', () => {
         service.active = true;
         service.vars = {};
         mockedRow.hasNext = jest.fn(() => false);
-        jest.spyOn(project, 'getOrCreateDataTable').mockReturnValueOnce(mockedTable);
         var validateProjectVatiables = require('./validateProjectVariables');
         jest.mock('./validateProjectVariables');
-
+        
         require('./repayments');
         
         expect(validateProjectVatiables).toHaveBeenCalledWith('prod');
