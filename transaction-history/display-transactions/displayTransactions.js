2adbde51d890a487dbf594e976a98d8d36cf6b8b
sabin sheja <sabin.sheja@oneacrefund.org>
2020-12-11 15:37:53 +0200

    changed the date format

--- transaction-history/display-transactions/displayTransactions.js
+++ transaction-history/display-transactions/displayTransactions.js
@@ -6,19 +6,16 @@ var listTransactions = function (transactionHistory, page, errorMessage) {
     var translate = createTranslator(translations, language);
     var offset = ((page || 1)-1) * 4;
     var to_show = transactionHistory.slice(offset, offset + 4);
-    var prev = translate('prev_page') +'\n';
-    if(offset == 0){
-        prev = '';
-    }
     var options_list = (errorMessage || '')+ translate('select_payment_detail_prompt')+'\n';
-    options_list = options_list + prev;
     to_show.forEach(function (transaction, index) {
-        var date = transaction.RepaymentDate.split(' ')[0].replace(/\//g, '-');
+        //var date = transaction.RepaymentDate.split(' ')[0].replace(/\//g, '-');
+        var date = new Date(transaction.RepaymentDate).toISOString().split('T')[0];
         options_list = options_list + translate('payment_list_item', {
             '$option': offset + index + 1,
             '$date': date,
             '$amount': transaction.Amount
         }) + '\n';
+        console.log('dat******************************'+date);
     });
     options_list = options_list + translate('continue');
     sayText(options_list);
