afe35600ad84fec3fa8b354d3fea61b52049df68
BAHATI Robben <bahatiroben@gmail.com>
2021-01-14 16:56:53 +0200

    adding the service veriables

--- ivr-repayment/ivrRepaymant.test.js
+++ ivr-repayment/ivrRepaymant.test.js
@@ -6,6 +6,18 @@ jest.mock('./validateClient');
 jest.mock('../shared/rosterApi/rosterColRequest');
 
 describe('ivr repayment', () => {
+    beforeAll(() => {
+        global.project.vars = {
+            dev_server_name: 'dev.server.local',
+            prod_server_name: 'prod.server.remote',
+            dev_roster_read_key: 'dev.roster_read_key',
+            prod_roster_read_key: 'prod.roster_read_key'
+        };
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
     it('should return false if the client is not validated', () => {
         validateClient.mockReturnValueOnce(false);
         const response = ivrRepayment('12345678', '250', '0789556765');
@@ -24,5 +36,22 @@ describe('ivr repayment', () => {
         triggerPayment.mockReturnValueOnce(true);
         const response = ivrRepayment('12345678', '250', '0789556765');
         expect(response).toBeTruthy();
+    });
+
+    it('should set the server name service variable to a development variable if the service is not active ', () => {
+        service.vars = {};
+        service.active = false;
+        validateClient.mockReturnValueOnce(false);
+        ivrRepayment('12345678', '250', '0789556765');
+        expect(service.vars.server_name).toEqual('dev.server.local');
+        expect(service.vars.roster_read_key).toEqual('dev.roster_read_key');
+    });
+
+    it('should set the server name service variable to a development variable if the service is not active ', () => {
+        service.active = true;
+        validateClient.mockReturnValueOnce(false);
+        ivrRepayment('12345678', '250', '0789556765');
+        expect(service.vars.server_name).toEqual('prod.server.remote');
+        expect(service.vars.roster_read_key).toEqual('prod.roster_read_key');
     });
 });
--- ivr-repayment/ivrRepayment.js
+++ ivr-repayment/ivrRepayment.js
@@ -2,6 +2,23 @@ var validateClient = require('./validateClient');
 var triggerPayment = require('../shared/rosterApi/rosterColRequest');
 
 module.exports = function(phoneNumber, paymentAmount, accountNumber) {
+    var defaultEnvironment;
+    if(service.active){
+        defaultEnvironment = 'prod';
+    }else{
+        defaultEnvironment = 'dev';
+    }
+
+    var env;
+    if(service.vars.env === 'prod' || service.vars.env === 'dev'){
+        env = service.vars.env;
+    }else{
+        env = defaultEnvironment;
+    }
+
+
+    service.vars.server_name = project.vars[env + '_server_name'];
+    service.vars.roster_read_key =  project.vars[env + '_roster_read_key'];
     var client = validateClient(accountNumber);
     if(client) {
         var paymentTriggered = triggerPayment(accountNumber, paymentAmount, phoneNumber);
