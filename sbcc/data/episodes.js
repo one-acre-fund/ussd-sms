3d5c7609db0f11a316db9b6bc93b73aaea291801
Victor Bassey <viktorbasi545@gmail.com>
2021-01-19 11:41:35 +0200

    add episodes and top tips

---
+++ sbcc/data/episodes.js
@@ -0,0 +1,50 @@
+module.exports = [
+    {
+        'name': 'episode-1',
+        'releaseDate': '01-25-2021'
+    },
+    {
+        'name': 'episode-2',
+        'releaseDate': '01-29-2021'
+    },
+    {
+        'name': 'episode-3',
+        'releaseDate': '02-08-2021'
+    },
+    {
+        'name': 'episode-4',
+        'releaseDate': '08-22-2021'
+    },
+    {
+        'name': 'episode-5',
+        'releaseDate': '03-08-2021'
+    },
+    {
+        'name': 'episode-6',
+        'releaseDate': '03-22-2021'
+    },
+    {
+        'name': 'episode-7',
+        'releaseDate': '04-07-2021'
+    },
+    {
+        'name': 'episode-8',
+        'releaseDate': '04-19-2021'
+    },
+    {
+        'name': 'episode-9',
+        'releaseDate': '05-03-2021'
+    },
+    {
+        'name': 'episode-10',
+        'releaseDate': '05-17-2021'
+    },
+    {
+        'name': 'episode-11',
+        'releaseDate': '05-31-2021'
+    },
+    {
+        'name': 'episode-12',
+        'releaseDate': '06-14-2021'
+    }
+];
