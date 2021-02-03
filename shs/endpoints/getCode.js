f78753e305beb56311e17216fe83468a6d2b23c5
sabin sheja <sabin.sheja@oneacrefund.org>
2021-02-03 15:59:19 +0200

    add mock data

--- shs/endpoints/getCode.js
+++ shs/endpoints/getCode.js
@@ -1,6 +1,58 @@
 
 module.exports = function (requestData){
     console.log('requetData:'+ JSON.stringify(requestData));
+
+    //double unlock
+    if((requestData.accountNumber == '30442923' || requestData.accountNumber == '15640569') && requestData.keyCodeType == 'unlock'){
+        return [
+            {
+                'unitType': 'biolite',
+                'unitSerialNumber': '11275434',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'unlock'
+            },
+            {
+                'unitType': 'sunking',
+                'unitSerialNumber': '64875628',
+                'keyCode': '123 466 799',
+                'keyCodeType': 'unlock'
+            }
+        ];
+    }
+    //single Unlock
+    else if((requestData.accountNumber == '20885173' || requestData.accountNumber == '15640569') && requestData.keyCodeType == 'unlock'){
+        return [{
+            'unitType': 'sunking',
+            'unitSerialNumber': '64875628',
+            'keyCode': '123 466 799',
+            'keyCodeType': 'unlock'
+        }];
+
+    }
+
+    //single activation
+    else if((requestData.accountNumber == '31619063' || requestData.accountNumber == '29752758') && requestData.keyCodeType == 'activation'){
+        return [{
+            'unitType': 'sunking',
+            'unitSerialNumber': '64875628',
+            'keyCode': '123 466 799',
+            'keyCodeType': 'activation'
+        }];
+    }
+    //double activation
+    else if((requestData.accountNumber == '31483941' || requestData.accountNumber == '29752758') && requestData.keyCodeType == 'activation'){
+        return [{
+            'unitType': 'sunking',
+            'unitSerialNumber': '16226110',
+            'keyCode': '677 560 290',
+            'keyCodeType': 'activation'
+        },{
+            'unitType': 'sunking',
+            'unitSerialNumber': '19748766',
+            'keyCode': '245 457 739',
+            'keyCodeType': 'activation'
+        }];
+    }
     return [
         {
             'unitType': 'biolite',
--- shs/endpoints/register-serial.js
+++ shs/endpoints/register-serial.js
@@ -4,15 +4,94 @@
 module.exports = function (requestData){
     console.log('requetData:'+ JSON.stringify(requestData));
 
-    if(requestData.unitType == 'biolite' || requestData.unitType == 'sunking' ){
+
+    if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647)) && requestData.unitType == 'biolite' && requestData.keyCodeType == 'unlock'){
         return [
             {
-                'unitType': requestData.unitType,
-                'unitSerialNumber': '23456789',
+                'unitType': 'biolite',
+                'unitSerialNumber': '13146227',
                 'keyCode': '123 456 789',
+                'keyCodeType': 'unlock'
+            }];
+    }
+    else if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647))&& requestData.unitType == 'sunking' && requestData.keyCodeType == 'activation'){
+        return [
+            {
+                'unitType': 'sunking',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 466 799',
                 'keyCodeType': 'activation'
             }
         ];
+    }
+    if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647)) && requestData.unitType == 'biolite' && requestData.keyCodeType == 'activation'){
+        return [
+            {
+                'unitType': 'biolite',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'activation'
+            }];
+    }
+    else if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647)) && requestData.unitType == 'sunking' && requestData.keyCodeType == 'unlock'){
+        return [
+            {
+                'unitType': 'sunking',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 466 799',
+                'keyCodeType': 'unlock'
+            }
+        ];
+    }
+
+    else if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647))  && requestData.keyCodeType == 'unlock'){
+        return [
+            {
+                'unitType': 'biolite',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'unlock'
+            },
+            {
+                'unitType': 'sunking',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 466 799',
+                'keyCodeType': 'unlock'
+            }
+        ];  
+    }
+    else if(((requestData.unitSerialNumber == 13246227) || (requestData.unitSerialNumber == 10966520) || (requestData.unitSerialNumber == 14581647))  && requestData.keyCodeType == 'activation'){
+        return [
+            {
+                'unitType': 'biolite',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'activation'
+            },
+            {
+                'unitType': 'sunking',
+                'unitSerialNumber': '13146227',
+                'keyCode': '123 466 799',
+                'keyCodeType': 'activation'
+            }
+        ];  
+    }
+    else{
+        if(requestData.keyCodeType == 'activation'){
+            return [{
+                'unitType': 'biolite',
+                'unitSerialNumber': '23456789',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'activation'
+            }];
+        }else if(requestData.keyCodeType == 'unlock'){
+            return [{
+                'unitType': 'biolite',
+                'unitSerialNumber': '23456789',
+                'keyCode': '123 456 789',
+                'keyCodeType': 'unlock'
+            }];
+        }
     }
     return [
         {
@@ -20,12 +99,6 @@ module.exports = function (requestData){
             'unitSerialNumber': '23456789',
             'keyCode': '123 456 789',
             'keyCodeType': 'activation'
-        },
-        {
-            'unitType': 'sunking',
-            'unitSerialNumber': '23456789',
-            'keyCode': '123 466 799',
-            'keyCodeType': 'unlock'
         }
     ];
 
