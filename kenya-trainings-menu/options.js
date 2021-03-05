1362578cd7d87392307c4972419f6e1f4ca36a9c
BAHATI Robben <bahatiroben@gmail.com>
2021-03-05 15:59:51 +0200

    switching on the kenya impact trainings

--- kenya-trainings-menu/options.js
+++ kenya-trainings-menu/options.js
@@ -2,52 +2,63 @@ module.exports = function () {
     return {
         'tree_transplanting': {
             'en-ke': 'Tree Transplanting\n',
-            'sw': 'Kupanda Miti\n'
+            'sw': 'Kupanda Miti\n',
+            enabled: true
         },
         'tree_bag_planting': {
             'en-ke': 'Tree Bag Planting\n',
-            'sw': 'Kupanda miti mifukoni\n'
+            'sw': 'Kupanda miti mifukoni\n',
+            enabled: true
         },
         'tree_socketing': {
             'en-ke': 'Tree Socketing\n',
-            'sw': 'Socketing Miti\n'
+            'sw': 'Socketing Miti\n',
+            enabled: true
         },
         'sorghum_weeding': {
             'en-ke': 'Sorghum Weeding\n',
-            'sw': 'Kupalilia Wimbi\n'
+            'sw': 'Kupalilia Wimbi\n',
+            enabled: true
         },
         'maize_topdress': {
             'en-ke': 'Maize Topdress\n',
-            'sw': 'TopDress\n'
+            'sw': 'TopDress\n',
+            enabled: true
         },
         'maize_intercorp': {
             'en-ke': 'Maize Intercrop\n',
-            'sw': 'Kupanda Mahindi/Maharagwe\n'
+            'sw': 'Kupanda Mahindi/Maharagwe\n',
+            enabled: true
         },
         'maize_harvest': {
             'en-ke': 'Maize Harvest\n',
-            'sw': 'Kuvuna Mahindi\n'
+            'sw': 'Kuvuna Mahindi\n',
+            enabled: true
         },
         'pest_mitigation': {
             'en-ke': 'Pest Mitigation\n',
-            'sw': 'Wadudu/Magonjwa\n'
+            'sw': 'Wadudu/Magonjwa\n',
+            enabled: true
         },
         'vegetables': {
             'en-ke': 'Vegetables\n',
-            'sw': 'Kupanda Mboga\n'
+            'sw': 'Kupanda Mboga\n',
+            enabled: true
         },
         'tatu_hadi_tatu': {
             'en-ke': 'Tatu Hadi Tatu\n',
-            'sw': 'Tatu Hadi Tatu\n'
+            'sw': 'Tatu Hadi Tatu\n',
+            enabled: true
         },
         'soil_training': {
             'en-ke': 'Soil Fertility\n',
             'sw': 'Udongo bora ulio na afya na rotuba\n',
-            enabled: false
+            enabled: true
         },
         'nutrition_training': {
             'en-ke': 'Dietary Diversity (Nutrition)\n',
-            'sw': 'Lishe Bora\n'
+            'sw': 'Lishe Bora\n',
+            enabled: true
         },
     };
 };
