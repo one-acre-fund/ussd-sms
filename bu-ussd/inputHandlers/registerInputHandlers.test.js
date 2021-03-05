d449c70cef9ff385153eaee8bd52b146e0c6ace4
BAHATI Robben <bahatiroben@gmail.com>
2021-03-05 13:15:05 +0200

    adding the register input handlers unit tests

---
+++ bu-ussd/inputHandlers/registerInputHandlers.test.js
@@ -0,0 +1,30 @@
+const registerInputHandlers = require('./registerInputHandlers');
+const {registerInputHandlers: registerRegistrationInputHandlers} = require('../registration/registration');
+
+jest.mock('../registration/registration');
+let onAccountNumberValidated;
+describe('register input handlers', () => {
+    beforeAll(() => {
+        onAccountNumberValidated = jest.fn();
+    });
+    it('should register splash input handler', () => {
+        const splashInputHandler = require('./splashInputHandler');
+        const handlerMock = jest.fn();
+        jest.spyOn(splashInputHandler, 'getHAndler').mockReturnValue(handlerMock);
+        registerInputHandlers('en-bu', onAccountNumberValidated);
+        expect(addInputHandler).toHaveBeenCalledWith(splashInputHandler.handlerName, handlerMock);
+        expect(splashInputHandler.getHAndler).toHaveBeenCalledWith('en-bu', onAccountNumberValidated);
+    });
+    it('should register main menu input handler', () => {
+        const mainMenuInputHandler = require('./mainMenuHandler');
+        const handlerMock = jest.fn();
+        jest.spyOn(mainMenuInputHandler, 'getHandler').mockReturnValue(handlerMock);
+        registerInputHandlers('en-bu', onAccountNumberValidated);
+        expect(addInputHandler).toHaveBeenCalledWith(mainMenuInputHandler.handlerName, handlerMock);
+        expect(mainMenuInputHandler.getHandler).toHaveBeenCalledWith('en-bu');
+    });
+    it('should register the registration input handlers', () => {
+        registerInputHandlers('en-bu', onAccountNumberValidated);
+        expect(registerRegistrationInputHandlers).toHaveBeenCalledWith('en-bu');
+    });
+});
