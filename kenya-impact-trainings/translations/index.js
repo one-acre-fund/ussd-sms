24b18cdf65e89760e61bd72cefe9ad1e018364e9
BAHATI Robben <39488016+Bahatiroben@users.noreply.github.com>
2021-03-23 14:02:25 +0200

    updating the trainings popup (#308)

--- kenya-impact-trainings/translations/index.js
+++ kenya-impact-trainings/translations/index.js
@@ -5,6 +5,6 @@ module.exports = {
     },
     'trainings_welcome_message': {
         'en': 'Congratulations $name on your first step towards becoming a smart farmer! A training SMS has been sent to your phone. Start your learning journey now!',
-        'sw': 'Hongera $name kwa hatua yako ya kwanza kuelekea kuwa mkulima mwerevu! Ujumbe wa mafunzo umetumwa kwa simu yako. Anza safari yako ya kujifunza sasa!'
+        'sw': 'Hongera $name kwa hatua yako ya kwanza kuelekea kuwa mkulima mwenye maarifa! Ujumbe wa mafunzo umetumwa kwa simu yako. Anza safari yako ya kujifunza sasa.'
     }
 };
