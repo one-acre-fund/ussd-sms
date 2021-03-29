6ba527b077cb1d0d961d409359013b63808d1d13
BAHATI Robben <bahatiroben@gmail.com>
2021-03-29 16:57:53 +0200

    updating the splash message

--- malawi/inputHandlers/accountNumberInputHandler.js
+++ malawi/inputHandlers/accountNumberInputHandler.js
@@ -1,5 +1,7 @@
 var getClient = require('../utils/getClient');
 var onAccountNumberValidated = require('../utils/onAccountNumberValidated');
+var translations = require('../translations/index');
+var translator = require('../../utils/translator/translator');
 var accountNumberInputHandler = 'mw_account_number_handler';
 
 module.exports = {
@@ -7,15 +9,11 @@ module.exports = {
     getHandler: function(lang) {
         return function(input){
             var account_number = input.replace(/\D/g, '');
-
+            var getMessage = translator(translations, lang);
             var client = getClient(account_number);
             if(client.error_message){
-                sayText(client.error_message);
-                promptDigits('account_number', {
-                    'submitOnHash': false,
-                    'maxDigits': 8,
-                    'timeout': 10,
-                });
+                global.sayText(getMessage('invalid_account', {}, lang));
+                global.promptDigits('account_number');
             } else {
                 onAccountNumberValidated(lang, client);
             } 
