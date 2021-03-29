8b1257c876a9852dcf7bc0b14caa26c4935ec1c0
BAHATI Robben <bahatiroben@gmail.com>
2021-03-29 10:26:34 +0200

    adding unit tests for next balance handler

--- malawi/checkBalance/inputHandlers/nextScreenBalance.js
+++ malawi/checkBalance/inputHandlers/nextScreenBalance.js
@@ -11,11 +11,11 @@ module.exports = {
             var balanceScreens = JSON.parse(state.vars.balance_screens);
             var currentSeasonBalance = parseInt(state.vars.current_season_balance);
             var balanceMenu = getMessage('balance_menu', {}, lang);
-            if(choice == '1') {
+            if(choice == '1' && (Object.keys(balanceScreens).length !== currentSeasonBalance)) {
                 // next season
                 state.vars.current_season_balance = currentSeasonBalance + 1;
-                global.sayText(balanceScreens[currentSeasonBalance] + balanceMenu);
-                global.promptDigits(balanceScreens[state.vars.current_season_balance]);
+                global.sayText(balanceScreens[state.vars.current_season_balance] + balanceMenu);
+                global.promptDigits(handlerName);
             } else if(choice == '2') {
                 // send message
                 global.sendMessage({
