0cebf49c441c8fc4fcfbc8bae8c7effb8219f735
BAHATI Robben <bahatiroben@gmail.com>
2021-05-06 12:01:15 +0200

    debugginng rwanda issue

--- rw-legacy/lib/populate-input-menu-by-district.js
+++ rw-legacy/lib/populate-input-menu-by-district.js
@@ -11,24 +11,24 @@ var msgs = require('./msg-retrieve');
 var bundles = getBundles();
 
 function countOptions(){
-    const count ={}
+    var count ={};
     bundles.forEach(function(bundle){
-        count[bundle.option_number] = 1 + (count[bundle.option_number] || 0)
-    })
-    return count
+        count[bundle.option_number] = 1 + (count[bundle.option_number] || 0);
+    });
+    return count;
 }
 
 module.exports = function( lang, max_chars){
-    var lang = lang || project.vars.lang;
+    lang = lang || project.vars.lang;
     var console_lang = project.vars.console_lang;
     var prev_page = msgs('prev_page',{},lang);
     var next_page = msgs('next_page',{},lang);
-    console.log(prev_page+"****************"+ next_page);
+    console.log(prev_page+'****************'+ next_page);
     
     if(prev_page == null || next_page == null){
-        var lang = 'ki';
-        var console_lang = 'en';
-        admin_alert = require('./admin-alert');
+        lang = 'ki';
+        console_lang = 'en';
+        var admin_alert = require('./admin-alert');
         console.log('error');
         admin_alert('Language not found\nError: ');
     } 
@@ -42,18 +42,18 @@ module.exports = function( lang, max_chars){
     for(var x = 1; x <= Object.keys(option_numbers).length; x++){
         
         try{
-            var row = bundles.filter(function (r) { return r.option_number === x; })[0]
-            if(row && row["d"+state.vars.client_districtId] == 1){
-                var temp_out = output + row['option_number']+ ")" + row[lang] + '\n';
+            var row = bundles.shift(); // old implementation was (.filter(function (r) { return r.option_number === x;})[0]) which omited bundles with option_number > loop count (x)
+            if(row && row['d'+state.vars.client_districtId] == 1){
+                var temp_out = output + row['option_number']+ ')' + row[lang] + '\n';
                 if(temp_out.length < max_chars){
-                    output = output + row['option_number']+ ")" + row[lang] + '\n';
+                    output = output + row['option_number']+ ')' + row[lang] + '\n';
                 }
                 else{
                     out_obj[loc] = output + next_page;
-                    output = prev_page + '\n' + row['option_number'] + ")" + row[lang] + '\n'
+                    output = prev_page + '\n' + row['option_number'] + ')' + row[lang] + '\n';
                     loc = loc + 1;
                 }
-                console_output = console_output + row['option_number'] + ")" + row[console_lang] + '\n';
+                console_output = console_output + row['option_number'] + ')' + row[console_lang] + '\n';
             }
         }
         catch(error){
@@ -62,12 +62,14 @@ module.exports = function( lang, max_chars){
             break;
         }
     }
-    console.log('output: ' + JSON.stringify({data: out_obj}) + 'lang: ' + lang)
+    console.log('output: ' + JSON.stringify({data: out_obj}) + 'lang: ' + lang);
     if(Object.keys(out_obj).length > 0){
         out_obj[loc] = out_obj[loc] = output;
+        console.log('output obje: ' + JSON.stringify({obje: out_obj}));
         return out_obj;
     }
     else{
+        console.log('output only: ' + JSON.stringify({output: output}));
         return output;
     }
-}
+};
