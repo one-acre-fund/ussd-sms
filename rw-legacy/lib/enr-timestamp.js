62791533377189844d75d283da0edb69acf74de8
BAHATI Robben <bahatiroben@gmail.com>
2021-05-11 10:24:46 +0200

    debugging

--- rw-legacy/lib/enr-timestamp.js
+++ rw-legacy/lib/enr-timestamp.js
@@ -7,7 +7,7 @@ module.exports = function(){
     var get_client = require('./enr-retrieve-client-row');
     var settings_table = project.initDataTableById(service.vars.ussd_settings_table_id);
     const an_pool = settings_table.queryRows({'vars' : {'settings' : 'enr_client_pool_21A'}}).next().vars.value;
-
+console.log('>>>>>>>>> reached settings: ' + settings_table)
     // calculate interaction time and save in client row
     var end_date = new Date().toString();
     var interaction_time = Math.abs(end_date - state.vars.start_date) / 1000; // convert from milliseconds to seconds
