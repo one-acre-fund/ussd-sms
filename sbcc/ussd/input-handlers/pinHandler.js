bcacb98c14700a59fc87b3c0a9fd33adf54b1b8f
Victor Bassey <viktorbasi545@gmail.com>
2021-05-28 16:41:11 +0200

    rename ussd end time var

--- sbcc/ussd/input-handlers/pinHandler.js
+++ sbcc/ussd/input-handlers/pinHandler.js
@@ -22,7 +22,7 @@ module.exports = function pinHandler(input) {
             to_number: contact.phone_number,
             route_id: routeId,
         });
-        contact.vars.sbcc_ussd_end_time = new Date(Date.now()).toString();
+        contact.vars.sbcc_ussd_ended_at = new Date(Date.now()).toString();
     } else {
         state.vars.incorrectPinAttempts += 1;
         if (state.vars.incorrectPinAttempts < 3) {
