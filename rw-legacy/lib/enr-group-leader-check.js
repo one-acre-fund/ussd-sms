49dc723366909071f3829d4dba617570fc5e4ec3
Sabin Sheja <sabin.sheja@oneacrefund.org>
2021-06-04 15:20:09 +0200

    merge origin

--- rw-legacy/lib/enr-group-leader-check.js
+++ rw-legacy/lib/enr-group-leader-check.js
@@ -6,26 +6,32 @@
 module.exports = function(client, glus_id, an_table_name, glus_table_name){
     var an_table = project.getOrCreateDataTable(an_table_name);
     var glus_table = project.getOrCreateDataTable(glus_table_name);
-    var group = glus_table.queryRows({vars : {'glus_id' : glus_id}}).next();
-    var group_leader = an_table.queryRows({vars : {'glus' : glus_id, 'group_leader' : 1}})
-    var client = an_table.queryRows({vars : {'account_number' : client}}).next(); //assumes that this client has been saved already
-    state.vars.needs_name = false; 
-    // assign client to be a group leader if there are no other group leaders in the group; otherwise, assign to be member
-    if(group_leader.count() < 1){
-        console.log('in gl check: vars: ' + JSON.stringify(client.vars) + '\ngl check count : ' + group_leader.count());
-        client.vars.group_leader = 1;
-        client.save();
-        console.log('gl? (line18): ' + client.vars.group_leader);
-        // if the group name is empty, needs_name is true
-        group = glus_table.queryRows({vars : {'glus_id' : glus_id, 'group_name' : {exists : 1}}});
-        if(group.count() < 1){
-            state.vars.needs_name = true;
+    var group_leader = null;
+    var group = {count: 0};
+    if(glus_id != null){
+        group = glus_table.queryRows({vars : {'glus_id' : glus_id}}).next();
+        group_leader = an_table.queryRows({vars : {'glus' : glus_id, 'group_leader' : 1}})
+
+        var client = an_table.queryRows({vars : {'account_number' : client}}).next(); //assumes that this client has been saved already
+        state.vars.needs_name = false; 
+        // assign client to be a group leader if there are no other group leaders in the group; otherwise, assign to be member
+        if(group_leader.count() < 1){
+            console.log('in gl check: vars: ' + JSON.stringify(client.vars) + '\ngl check count : ' + group_leader.count());
+            client.vars.group_leader = 1;
+            client.save();
+            console.log('gl? (line18): ' + client.vars.group_leader);
+            // if the group name is empty, needs_name is true
+            group = glus_table.queryRows({vars : {'glus_id' : glus_id, 'group_name' : {exists : 1}}});
+            if(group.count() < 1){
+                state.vars.needs_name = true;
+            }
+            return true;
         }
-        return true;
+        else{
+            //client.vars.group_leader = 0;
+            //client.save();
+            return false;
+        }
     }
-    else{
-        //client.vars.group_leader = 0;
-        //client.save();
-        return false;
-    }
+    return false;
 };
