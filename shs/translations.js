784596d65479b47b12c7c98e643c814c4d70d735
BAHATI Robben <bahatiroben@gmail.com>
2021-06-22 12:40:36 +0200

    adding the kinyarwanda translations

--- shs/translations.js
+++ shs/translations.js
@@ -2,126 +2,157 @@ module.exports = {
 
     'select_service': {
         'en': 'What do you want to do?\n1)Register New SHS Unit \n2)Get Activation/ Unlock Code\n3)View Recent Activation/Unlock Code \n4)Back',
-        'sw': 'Huduma gani unahitaji?\n1)Sajili Sola yako\n2)Pata Kodi ya sola yako\n3)Angalia kodi ya Sola yako \n4)rudi nyuma'
+        'sw': 'Huduma gani unahitaji?\n1)Sajili Sola yako\n2)Pata Kodi ya sola yako\n3)Angalia kodi ya Sola yako \n4)rudi nyuma',
+        'ki': 'Urifuza gukora iki?:\n1) Kwandikisha itara rishya\n2)Gusaba kode\n3) Kureba kode zose wahawe\n4)Gusubira inyuma'
     },
     'serial_number_request': {
         'en': 'Enter SHS Serial Number',
-        'sw': 'Ingiza Serial number ya Sola yako'
+        'sw': 'Ingiza Serial number ya Sola yako',
+        'ki': 'Shyiramo numero iranga itara:\n1)Itara rishya\n2) Gusimbuza itara'
     },
     'valid_shs_message': {
         'en': 'Thank you for registering your SHS Unit. You will be receiving an activation code shortly if you are eligible.',
-        'sw': 'Asante kwa kusajili taa yako. Utapokea kodi ya kuwasha taa kwa muda mfupi iwapo uliagiza  taa kwa mkopo wako.'
+        'sw': 'Asante kwa kusajili taa yako. Utapokea kodi ya kuwasha taa kwa muda mfupi iwapo uliagiza  taa kwa mkopo wako.',
+        'ki': 'Urakoze kwandikisha itara. Urakira imibare yo gucana itara mu kanya'
     },
     'invalid_shs_message': {
         'en': 'You have entered an invalid serial number, please try again',
-        'sw': 'Nambari uliyoweka sio Sahihi, tafadhali weka Nambari sahihi'
+        'sw': 'Nambari uliyoweka sio Sahihi, tafadhali weka Nambari sahihi',
+        'ki': 'Wanditse imibare iranga itara nabi, ongera ugerageze'
     },
     'successful_activation_code': {
         'en': 'Your activation code is $code',
-        'sw': 'Kodi yako ni $code'
+        'sw': 'Kodi yako ni $code',
+        'ki': 'Umubare wo gucana itara ni: $code'
     },
     'successful_unlock_code': {
         'en': 'Your unlock code is $code',
-        'sw': 'Kodi yako ni $code'
+        'sw': 'Kodi yako ni $code',
+        'ki': 'Kode yawe ya burundu ni $code'
     },
     'successful_farmer_activation_code': {
         'en': 'The Client\'s activation code is $code',
-        'sw': 'Kodi ya mkulima ni $code'
+        'sw': 'Kodi ya mkulima ni $code',
+        'ki': 'kode y\' umukiriya ni $code'
     },
     'successful_farmer_unlock_code': {
         'en': 'The Client\'s unlock code is $code',
-        'sw': 'Kodi ya mkulima ni $code'
+        'sw': 'Kodi ya mkulima ni $code',
+        'ki': 'kode y\' umukiriya ni $code'
     },
     'unsuccessful_code_sms': {
         'en': 'You are not eligible to receive a code. Please place an order or call OAF for help',
-        'sw': 'Samahani, haukuagiza taa kwa mkopo wako, tafadhali piga simu kwa  customer care kwa usaidizi'
+        'sw': 'Samahani, haukuagiza taa kwa mkopo wako, tafadhali piga simu kwa  customer care kwa usaidizi',
+        'ki': 'Ntago uragera ku ntego y\'ibwishyu, banza wishyure cyangwa uhamagare 2580 bagufashe'
     },
     'error': {
         'en': 'an error occured, please try again later',
         'sw': 'an error occured, please try again later',
+        'ki': 'Habayemo ikibazo, mwongere mukanya',
     },
     'shs_type': {
         'en': 'Choose SHS Unit Type\n $serialTypes',
-        'sw': 'Chagua aina ya Sola\n $serialTypes' 
+        'sw': 'Chagua aina ya Sola\n $serialTypes',
+        'ki': 'Hitamo ubwoko bw\' itara $serialTypes'
     },
     'recent_code': {
         'en': 'Your most recent activation code is $code',
-        'sw': 'Kodi yako ya hivi karibuni ni $code'
+        'sw': 'Kodi yako ya hivi karibuni ni $code',
+        'ki': 'Kode z\'itara ryawe ni $code'
     },
     'no_recent_code': {
         'en': 'You do not  have an activation code',
-        'sw': 'Samahani, kodi ya taa yako haipo'
+        'sw': 'Samahani, kodi ya taa yako haipo',
+        'ki': 'Nta kodi yitara mufite'
     },
     'gl_menu': {
         'en': 'Select a Service\n1) My SHS Unit\n2)SHS Unit for another Client\n3) Back',
-        'sw': 'Chagua Huduma\n1)Sola yangu\n2)Sola ya mkulima mwingine\n3)Rudi nyuma'
+        'sw': 'Chagua Huduma\n1)Sola yangu\n2)Sola ya mkulima mwingine\n3)Rudi nyuma',
+        'ki': 'Hitamo itara wifuza kureba:\n1) Itara ryanjye\n2)Umuhinzi wundi\n3) Gusubira inyuma'
     },
     'account_number_menu': {
         'en': 'Enter Client Account Number',
-        'sw': 'Weka Akunti Namba ya Mkulima'
+        'sw': 'Weka Akunti Namba ya Mkulima',
+        'ki': 'Shyiramo numero ya konti y\'umuhinzi'
     },
     'serial_numbers': {
         'en': 'Request activation/Unlock  code for serial number\n $serialNumbers \n99) None of the above',
-        'sw': 'Agiza kodi ya sola yako\n $serialNumbers \n99) Nambari sio zangu'
+        'sw': 'Agiza kodi ya sola yako\n $serialNumbers \n99) Nambari sio zangu',
+        'ki': 'Gusaba kode, shyiramo:\n$serialNumbers\n99) Ntanimwe'
     },
     'serial_number_request_menu': {
         'en': 'Enter Serial Number of SHS Unit',
-        'sw': 'Ingiza Serial number ya Sola yako' 
+        'sw': 'Ingiza Serial number ya Sola yako',
+        'ki': ' Shyiramo numero iranga itara ryawe'
     },
     'register_serial_menu': {
         'en': 'Enter SHS Serial Number\n1) New SHS Unit\n2) Replacement through warranty',
-        'sw': 'Ingiza Serial number ya Sola yako\n1) Sola Mpya\n2) Sola ambayo Imebadilishwa kwa warranty'
+        'sw': 'Ingiza Serial number ya Sola yako\n1) Sola Mpya\n2) Sola ambayo Imebadilishwa kwa warranty',
+        'ki': 'Shyiramo numero iranga itara:\n1)Itara rishya\n2) Gusimbuza itara'
     },
     'no_serial_message': {
         'en': 'You are not eligible to receive a code. Please place an order or call OAF  for help',
-        'sw': 'Samahani, haukuagiza taa kwa mkopo wako, tafadhali piga simu kwa  customer care kwa usaidizi'
+        'sw': 'Samahani, haukuagiza taa kwa mkopo wako, tafadhali piga simu kwa  customer care kwa usaidizi',
+        'ki': 'Ntago uragera ku ntego y\'ibwishyu, banza wishyure cyangwa uhamagare 2580 bagufashe'
     },
     'replacement_menu': {
         'en': 'Choose the device you want to replace\n $serialNumbers',
-        'sw': 'Chagua taa unayotaka kubadilisha serial number\n $serialNumbers'
+        'sw': 'Chagua taa unayotaka kubadilisha serial number\n $serialNumbers',
+        'ki': 'Hitamo itara ushaka gusimbuza\n $serialNumbers'
     },
     'serial_numbers_replace': {
         'en': 'Request activation/Unlock  code for serial number\n $serialNumbers',
-        'sw': 'Agiza kodi ya sola yako\n $serialNumbers'
+        'sw': 'Agiza kodi ya sola yako\n $serialNumbers',
+        'ki': 'Gusaba kode, shyiramo:\n$serialNumbers\n99) Ntanimwe'
     },
     'not_enrolled': {
         'en': 'The acccount number used did not place order in the current season',
-        'sw': 'Nambari ya akaunti iliyotumiwa haikuweka mpangilio katika msimu wa sasa'
+        'sw': 'Nambari ya akaunti iliyotumiwa haikuweka mpangilio katika msimu wa sasa',
+        'ki': 'Numero ya konti mwashyizemo ntago yigeze itumiza muri iki gihembwe'
     },
     'different_group': {
         'en': 'This Client is not in your group, you can only view SHS options for clients in your group',
-        'sw': 'Samahani mkulima hayuko kwenye kikundi chako'
+        'sw': 'Samahani mkulima hayuko kwenye kikundi chako',
+        'ki': 'Uyu muhizni ntago ari mu itsinda ryawe'
     },
     'invalid_account': {
         'en': 'The Account number you entered is invalid, please enter a valid account number',
-        'sw': 'Akaunti namba uliyoweka sio sahihi, tafadhali rudia tena.'
+        'sw': 'Akaunti namba uliyoweka sio sahihi, tafadhali rudia tena.',
+        'ki': 'Numero ya konti mwashyizemo si yo, mugerageze indi'
     },
     'view_recent_code': {
         'en': 'View Recent activation/unlock  code for serial number\n $serialNumbers \n99) None of the above',
-        'sw': 'Angalia kodi uliyopata hivi karibuni\n $serialNumbers \n99) Nambari sio zangu'
+        'sw': 'Angalia kodi uliyopata hivi karibuni\n $serialNumbers \n99) Nambari sio zangu',
+        'ki': 'Kureba kode zose wahawe, shyiramo\n$serialNumbers\n99) Ntanimwe'
     },
     'serial_assigned': {
         'en': 'The serial Number $serialNumber is assigned to another person',
-        'sw': 'Nambari ya $serialNumber imepewa mtu mwingine'
+        'sw': 'Nambari ya $serialNumber imepewa mtu mwingine',
+        'ki': 'Numero y\'itara $serialNumber ni iyundi muntu'
     },
     'internal_error': {
         'en': 'There is a system error. please try again later',
-        'sw': 'kuna hitilafu ya mfumo. tafadhali jaribu tena baadae'
+        'sw': 'kuna hitilafu ya mfumo. tafadhali jaribu tena baadae',
+        'ki': 'Hbayemo ikibazo. Ongera ugerageze mukanya'
     },
     'no_code_message': {
         'en': 'You do not  have an activation code, please register your SHS Unit',
-        'sw': 'Hauna kodi ya kuwasha sola, tafadhali sajili sola yako'
+        'sw': 'Hauna kodi ya kuwasha sola, tafadhali sajili sola yako',
+        'ki': 'Nta kodi yo gucana itara mufite, andikisha Itara ryawe'
     },
     'no_code_message_client': {
         'en': 'Client does not  have an activation code, please register Client SHS Unit',
-        'sw': 'Mkulima hana kodi, tafadhali sajili sola ya mkulima'
+        'sw': 'Mkulima hana kodi, tafadhali sajili sola ya mkulima',
+        'ki': 'Uyu muhinzi nta kodi yo gucana itara afite, banza wandikishe Itara rye'
     },
     'previous_loan_message': {
         'en': 'Kindly complete your loan to get your unlock codes',
-        'sw': 'Samahani, maliza mkopo wako ili kupokea kodi ya kuwasha taa milele'
+        'sw': 'Samahani, maliza mkopo wako ili kupokea kodi ya kuwasha taa milele',
+        'ki': 'Mubanze mwishyure ideni ryanyu kugirango mubone kodi ya burundu'
     },
     'valid_exists_shs_message': {
         'en': 'Your solar has already been registered. Your activation code is $code',
-        'sw': 'Sola yako imeshasajiliwa, na kodi yako ni $code'
+        'sw': 'Sola yako imeshasajiliwa, na kodi yako ni $code',
+        'ki': 'Itara ryawe ryaranditswe. Kodi yawe ni $code'
     }
 };
